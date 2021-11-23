5432044a475e58cbba81bd4843823f123b7fbd61
Li Huang <lihuang.dmft@gmail.com>
2021-11-23 16:46:06 +0800

    implement maxent

--- src/ACFlow.jl
+++ src/ACFlow.jl
@@ -3,6 +3,10 @@ module ACFlow
 
 include("global.jl")
 include("types.jl")
+include("inout.jl")
+include("maxent.jl")
+include("test.jl")
 
+export test_maxent
 
 end
---
+++ src/test.jl
@@ -0,0 +1,4 @@
+
+function test_maxent()
+    println("Hello world! This is a maxent code.")
+end
