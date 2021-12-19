7a0d0841aec159b6586d6a4e30815d4999713a71
Li Huang <lihuang.dmft@gmail.com>
2021-12-19 23:44:11 +0800

    add test mode

--- src/test.py
+++ src/test.py
@@ -5,6 +5,6 @@ import scipy.linalg
 A = numpy.array([[3.81631, 0.0, 1.6561],
                  [0.0, 7.9080, 0.0],
                  [1.6561, 0.0, 7.3933]])
-W, V = scipy.linalg.eigh(A)
+W, V = scipy.linalg.eig(A)
 #print(W)
 print(V)
