f0fda445dcf20cad423cc45239a615901e0c338a
Li Huang <lihuang.dmft@gmail.com>
2021-12-20 13:30:19 +0800

    update codes

---
+++ src/som.jl
@@ -0,0 +1,8 @@
+#
+# Project : Gardenia
+# Source  : som.jl
+# Author  : Li Huang (lihuang.dmft@gmail.com)
+# Status  : Unstable
+#
+# Last modified: 2021/12/20
+#
--- src/types.jl
+++ src/types.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (lihuang.dmft@gmail.com)
 # Status  : Unstable
 #
-# Last modified: 2021/12/16
+# Last modified: 2021/12/20
 #
 
 abstract type AbstractData end
