2e97ce10b4e97169164a2255adb6822783e9843a
Li Huang <lihuang.dmft@gmail.com>
2021-12-20 13:35:10 +0800

    update codes

--- src/ACFlow.jl
+++ src/ACFlow.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (lihuang.dmft@gmail.com)
 # Status  : Unstable
 #
-# Last modified: 2021/12/14
+# Last modified: 2021/12/20
 #
 
 module ACFlow
@@ -25,6 +25,7 @@ include("kernel.jl")
 include("spectrum.jl")
 include("pade.jl")
 include("maxent.jl")
+include("som.jl")
 
 export MomentsData
 export KernelData
