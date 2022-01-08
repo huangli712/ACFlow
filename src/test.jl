13de8df865c2720f6cac9f21df18fd5514c76784
Li Huang <lihuang.dmft@gmail.com>
2022-01-08 22:05:06 +0800

    update codes

--- src/test.jl
+++ src/test.jl
@@ -43,10 +43,10 @@ g0, 𝐺, τ, Mrot = read_data!(ImaginaryTimeGrid)
 #@show Grid2Spec(2000, grid)
 #error()
 
-SG, SE, SC, MC, kernel = init_sac(g0, 𝐺, τ, Mrot)
+#SG, SE, SC, MC, kernel = init_sac(g0, 𝐺, τ, Mrot)
 #SE = init_spectrum(g0, grid, 𝐺, τ)
 #kernel = init_kernel(τ, grid, Mrot)
 #compute_corr_from_spec(kernel, SE, SC)
 #@show typeof(kernel)
 
-sac_run(g0, MC, SE, SC, SG, kernel, 𝐺)
+sac_run(g0, 𝐺, τ, Mrot)
