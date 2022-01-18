36de4c1ba5feb15cd6ac9dede425ed68caeb06b0
Li Huang <lihuang.dmft@gmail.com>
2022-01-18 21:04:13 +0800

    add secant()

--- src/maxent.jl
+++ src/maxent.jl
@@ -186,6 +186,55 @@ function maxent_init(G::GreenData, mesh::MaxEntGrid, ω::FermionicMatsubaraGrid)
     return MaxEntContext(E, kernel, d2chi2, W2, W3, n_sv, V_svd, Evi, model, imdata)
 end
 
+function secant(func, x0, args)
+    eps = 1.0e-4
+    maxiter = 50
+    tol = 1.48e-8
+    funcalls = 0
+    p0 = 1.0 * x0
+    p1 = x0 * (1.0 + eps)
+    if p1 ≥ 0.0
+        p1 = p1 + eps
+    else
+        p1 = p1 - eps
+    end
+
+    q0 = func(p0, args)
+    funcalls = funcalls + 1
+    q1 = func(p1, args)
+    funcalls = funcalls + 1
+
+    if abs(q1) < abs(q0)
+        p0, p1 = p1, p0
+        q0, q1 = q1, q0
+    end
+
+    for itr = 1:maxiter
+        if q1 == q0
+            if p1 != p0
+                error("tolerance is reached!")
+            end
+            p = (p1 + p0) / 2.0
+            return p
+        else
+            if abs(q1) > abs(q0)
+                p = (-q0 / q1 * p1 + p0) / (1 - q0 / q1)
+            else
+                p = (-q1 / q0 * p0 + p1) / (1 - q1 / q0)
+            end
+        end
+
+        if abs(p - p1) < tol
+            return p
+        end
+
+        p0, q0 = p1, q1
+        p1 = p
+        q1 = func(p1, args)
+        funcalls = funcalls + 1
+    end
+end
+
 function maxent_run_historic(mec::MaxEntContext, mesh::MaxEntGrid)
     println("Solving")
     alpha = 10 ^ 6.0
