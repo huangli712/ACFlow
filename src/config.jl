463814818d7ae1db2f76b897252605e1b5b3ea09
Li Huang <lihuang.dmft@gmail.com>
2022-01-20 22:49:05 +0800

    fix PCOMM

--- src/config.jl
+++ src/config.jl
@@ -13,6 +13,7 @@ const PCOMM    = Dict{String,ADT}(
     "solver" => [missing, 1, :String, ""],
     "kernel" => [missing, 1, :String, ""],
     "model"  => [missing, 1, :String, ""],
+    "grid"   => [missing, 1, :String, ""],
     "mesh"   => [missing, 1, :String, ""],
     "wmax"   => [missing, 1, :F64, ""],
     "wmin"   => [missing, 1, :F64, ""],
