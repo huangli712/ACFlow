1b414b4f3e7c8192e974902ecec325f255e8ef94
Li Huang <lihuang.dmft@gmail.com>
2022-01-20 22:10:25 +0800

    add inp_toml()

--- src/config.jl
+++ src/config.jl
@@ -29,3 +29,50 @@ const PStochOM = Dict{String,ADT}(
 
 const PStochAC = Dict{String,ADT}(
 )
+
+
+"""
+    inp_toml(f::String, key::String, necessary::Bool)
+
+Parse the configuration file (in toml format). It reads only parts of
+the configuration file, which depends on the value of `key`.
+
+See also: [`setup`](@ref).
+"""
+function inp_toml(f::String, key::String, necessary::Bool)
+    if isfile(f)
+        dict = TOML.parsefile(f)
+
+        if haskey(dict, key)
+            dict[key]
+        else
+            error("Do not have this key: $key in file: $f")
+        end
+    else
+        if necessary
+            error("Please make sure that the file $f really exists")
+        else
+            nothing
+        end
+    end
+end
+
+"""
+    inp_toml(f::String, necessary::Bool)
+
+Parse the configuration file (in toml format). It reads the whole file.
+
+See also: [`setup`](@ref).
+"""
+function inp_toml(f::String, necessary::Bool)
+    if isfile(f)
+        dict = TOML.parsefile(f)
+        return dict
+    else
+        if necessary
+            error("Please make sure that the file $f really exists")
+        else
+            nothing
+        end
+    end
+end
