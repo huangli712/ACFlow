6ebd7831ff96d0498812af0abc25679373339805
Li Huang <lihuang.dmft@gmail.com>
2022-01-22 17:24:52 +0800

    fix types.jl

--- src/types.jl
+++ src/types.jl
@@ -1,4 +1,5 @@
 abstract type AbstractMesh end
+
 struct UniformMesh <: AbstractMesh
     nmesh :: I64
     wmax  :: F64
@@ -6,24 +7,35 @@ struct UniformMesh <: AbstractMesh
     mesh :: Vector{F64}
     weight :: Vector{F64}
 end
+
 struct NonUniformMesh <: AbstractMesh end
 
 abstract type AbstractGrid end
-struct FermionicImaginaryTimeGrid <: AbstractGrid end
+
+struct FermionicImaginaryTimeGrid <: AbstractGrid
+    ngrid :: I64
+    β :: F64
+    τ :: Vector{F64}
+end
+
 struct FermionicMatsubaraGrid <: AbstractGrid
     ngrid :: I64
     β :: F64
     ω :: Vector{F64}
 end
+
 struct BosonicImaginaryTimeGrid <: AbstractGrid end
+
 struct BosonicMatsubaraGrid <: AbstractGrid end
 
 abstract type AbstractData end
+
 struct RawData{T} <: AbstractData
     mesh  :: Vector{F64}
     value :: Vector{T}
     error :: Vector{T}
 end
+
 mutable struct GreenData <: AbstractData
     value :: Vector{C64}
     error :: Vector{F64}
