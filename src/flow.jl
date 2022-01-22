34bd1f323dc541da59385425ab8a9d1afd4ec4df
Li Huang <lihuang.dmft@gmail.com>
2022-01-22 09:16:48 +0800

    fix flow.jl

--- src/flow.jl
+++ src/flow.jl
@@ -1,2 +1,19 @@
 
 
+function read_param()
+    cfg = inp_toml(query_args(), true)
+    fil_dict(cfg)
+end
+
+function read_data()
+    finput = get_c("finput")
+    ngrid = get_c("ngrid")
+    grid = get_c("grid")
+    @show finput, ngrid, grid
+
+    if grid == "matsubara"
+        return read_matsubara_data(finput, ngrid)
+    else
+        return read_imaginary_time_data(finput, ngrid)
+    end
+end
--- src/inout.jl
+++ src/inout.jl
@@ -1,16 +1,5 @@
 
-function read_data()
-    finput = get_c("finput")
-    ngrid = get_c("ngrid")
-    grid = get_c("grid")
-    @show finput, ngrid, grid
 
-    if grid == "matsubara"
-        return read_matsubara_data(finput, ngrid)
-    else
-        return read_imaginary_time_data(finput, ngrid)
-    end
-end
 
 function read_imaginary_time_data(finput::String, ngrid::I64)
 end
