27753cc5a185d32cc4010859accd4e90fc0abd15
Li Huang <lihuang.dmft@gmail.com>
2022-01-22 17:10:59 +0800

    add make_kernel()

--- src/kernel.jl
+++ src/kernel.jl
@@ -1,1 +1,13 @@
+function make_kernel(um::UniformMesh, fg::FermionicMatsubaraGrid)
+    niw = fg.ngrid
+    nw = um.nmesh
 
+    kernel = zeros(C64, niw, nw)
+    for i = 1:nw
+        for j = 1:niw
+            kernel[j,i] = 1.0 / (im * fg[j] - um[i])
+        end
+    end
+
+    return kernel
+end
