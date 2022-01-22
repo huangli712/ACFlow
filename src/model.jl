5d08d58bce93edf1f02ce15864e2fdaf7b1b230e
Li Huang <lihuang.dmft@gmail.com>
2022-01-22 16:46:59 +0800

    add make_model()

--- src/model.jl
+++ src/model.jl
@@ -1,14 +1,19 @@
-function make_model()
+function make_model(um::UniformMesh)
     model = get_c["model"]
     if model == "flat"
-        return make_flat_model()
+        return make_flat_model(um)
     else
-        return make_gaussian_model()
+        return make_gaussian_model(um)
     end
 end
 
-function make_flat_model()
+function make_flat_model(um::UniformMesh)
+    model = ones(F64, um.nmesh)
+    norm = trapz(um.mesh, model)
+    model = model ./ norm
+
+    return FlatModel(um.nmesh, model)
 end
 
-function make_gaussian_model()
+function make_gaussian_model(um::UniformMesh)
 end
