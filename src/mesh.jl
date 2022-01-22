623701b3d6742e6a5ed516e987c9c741cf447a88
Li Huang <lihuang.dmft@gmail.com>
2022-01-22 12:08:00 +0800

    add make_uniform_mesh()

--- src/mesh.jl
+++ src/mesh.jl
@@ -1,3 +1,13 @@
 function make_uniform_mesh()
-    
+    nmesh = get_c["nmesh"]
+    wmax = get_c["wmax"]
+    wmin = get_c["wmin"]
+
+    mesh = collect(LinRange(wmin, wmax, nmesh))
+    weight = (mesh[2:end] + mesh[1:end-1]) / 2.0
+    pushfirst!(weight, mesh[1])
+    push!(weight, mesh[end])
+    weight = diff(weight)
+
+    return UniformMesh(nmesh, wmax, wmin, mesh, weight)
 end
