96b29a12c5d9af19d59d2d9fd6793310629649b9
Li Huang <lihuang.dmft@gmail.com>
2022-01-23 23:49:09 +0800

    fix types.jl

--- src/ACFlow.jl
+++ src/ACFlow.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (lihuang.dmft@gmail.com)
 # Status  : Unstable
 #
-# Last modified: 2022/01/22
+# Last modified: 2022/01/24
 #
 
 module ACFlow
--- src/types.jl
+++ src/types.jl
@@ -1,32 +1,46 @@
 abstract type AbstractGrid end
 
 struct FermionicImaginaryTimeGrid <: AbstractGrid
-    ngrid :: I64
+    ntime :: I64
     β :: F64
     τ :: Vector{F64}
 end
 
 struct FermionicMatsubaraGrid <: AbstractGrid
-    ngrid :: I64
+    nfreq :: I64
     β :: F64
     ω :: Vector{F64}
 end
 
-struct BosonicImaginaryTimeGrid <: AbstractGrid end
+struct BosonicImaginaryTimeGrid <: AbstractGrid
+    ntime :: I64
+    β :: F64
+    τ :: Vector{F64}
+end
 
-struct BosonicMatsubaraGrid <: AbstractGrid end
+struct BosonicMatsubaraGrid <: AbstractGrid
+    nfreq :: I64
+    β :: F64
+    ω :: Vector{F64}
+end
 
 abstract type AbstractMesh end
 
 struct UniformMesh <: AbstractMesh
     nmesh :: I64
-    wmax  :: F64
-    wmin  :: F64
+    wmax :: F64
+    wmin :: F64
     mesh :: Vector{F64}
     weight :: Vector{F64}
 end
 
-struct NonUniformMesh <: AbstractMesh end
+struct NonUniformMesh <: AbstractMesh
+    nmesh :: I64
+    wmax :: F64
+    wmin :: F64
+    mesh :: Vector{F64}
+    weight :: Vector{F64}
+end
 
 abstract type AbstractData end
 
