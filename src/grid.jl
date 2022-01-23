ab09d4cddbc3b8067e18d07a21ff9eacd87a8881
Li Huang <lihuang.dmft@gmail.com>
2022-01-23 23:53:04 +0800

    add getindex()

--- src/grid.jl
+++ src/grid.jl
@@ -9,6 +9,16 @@ function Base.getindex(fg::FermionicMatsubaraGrid, ind::I64)
     return fg.ω[ind]
 end
 
+function Base.getindex(bg::BosonicImaginaryTimeGrid, ind::I64)
+    @assert 1 ≤ ind ≤ bg.ntime
+    return bg.τ[ind]
+end
+
+function Base.getindex(bg::BosonicMatsubaraGrid, ind::I64)
+    @assert 1 ≤ ind ≤ bg.nfreq
+    return bg.ω[ind]
+end
+
 function make_grid(fg::FermionicMatsubaraGrid)
     for n in eachindex(fg.ω)
         fg.ω[n] = (2 * n - 1) * π / fg.β
