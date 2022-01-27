689c2406c794337ad55ab5d4821753593fd4f719
Li Huang <lihuang.dmft@gmail.com>
2022-01-27 23:58:12 +0800

    fix read_time_data()

--- src/inout.jl
+++ src/inout.jl
@@ -34,6 +34,20 @@ function write_spectrum(am::AbstractMesh, spectra::Vector{F64})
 end
 
 function read_time_data(finput::String, ngrid::I64)
+    mesh = zeros(F64, ngrid)
+    value = zeros(F64, ngrid)
+    error = zeros(F64, ngrid)
+
+    open(finput, "r") do fin
+        for i = 1:ngrid
+            arr = parse.(F64, line_to_array(fin)[1:3])
+            mesh[i] = arr[1]
+            value[i] = arr[2]
+            error[i] = arr[3] * 10.0
+        end
+    end
+
+    return RawData(mesh, value, error)
 end
 
 function read_freq_data(finput::String, ngrid::I64)
