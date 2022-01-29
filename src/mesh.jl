cdf78b3297f99d617a94eea4965bf0f2ffeeb815
Li Huang <lihuang.dmft@gmail.com>
2022-01-29 18:15:25 +0800

    fix mesh.jl

--- src/mesh.jl
+++ src/mesh.jl
@@ -17,6 +17,23 @@ mutable struct UniformMesh <: AbstractMesh
     weight :: Vector{F64}
 end
 
+function UniformMesh(nmesh::I64, wmin::F64, wmax::F64)
+    @assert nmesh ≥ 1
+    @assert wmax > wmin
+
+    mesh = collect(LinRange(wmin, wmax, nmesh))
+    weight = (mesh[2:end] + mesh[1:end-1]) / 2.0
+    pushfirst!(weight, mesh[1])
+    push!(weight, mesh[end])
+    weight = diff(weight)
+
+    return UniformMesh(nmesh, wmax, wmin, mesh, weight)
+end
+
+function Base.eachindex(um::UniformMesh)
+    eachindex(um.mesh)
+end
+
 function Base.firstindex(um::UniformMesh)
     firstindex(um.mesh)
 end
@@ -28,6 +45,16 @@ end
 function Base.getindex(um::UniformMesh, ind::I64)
     @assert 1 ≤ ind ≤ um.nmesh
     return um.mesh[ind]
+end
+
+function Base.getindex(um::UniformMesh, I::UnitRange{I64})
+    @assert checkbounds(Bool, um.mesh, I)
+    lI = length(I)
+    X = similar(um.mesh, lI)
+    if lI > 0
+        unsafe_copyto!(X, 1, um.mesh, first(I), lI)
+    end
+    return X
 end
 
 mutable struct NonUniformMesh <: AbstractMesh
@@ -43,31 +70,24 @@ function Base.getindex(num::NonUniformMesh, ind::I64)
     return num.mesh[ind]
 end
 
+
 function make_mesh()
     mesh = get_c("mesh")
-    if mesh == "uniform"
-        return build_uniform_mesh()
-    else
-        return build_non_uniform_mesh()
-    end
-end
-
-function build_uniform_mesh()
     nmesh = get_c("nmesh")
     wmax = get_c("wmax")
     wmin = get_c("wmin")
 
-    mesh = collect(LinRange(wmin, wmax, nmesh))
-    weight = (mesh[2:end] + mesh[1:end-1]) / 2.0
-    pushfirst!(weight, mesh[1])
-    push!(weight, mesh[end])
-    weight = diff(weight)
-
-    return UniformMesh(nmesh, wmax, wmin, mesh, weight)
+    if mesh == "uniform"
+        return UniformMesh(nmesh, wmin, wmax)
+    else
+        return NonUniformMesh(nmesh, wmin, wmax)
+    end
 end
 
-function build_non_uniform_mesh()
-end
+
+
+
+
 
 function trapz(x::Vector, y::Vector, uniform::Bool = false)
     if uniform
