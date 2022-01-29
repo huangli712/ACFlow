d7f013030e64f1274be290afef0d37bdcf7162f5
Li Huang <lihuang.dmft@gmail.com>
2022-01-29 11:23:46 +0800

    update code style

--- src/global.jl
+++ src/global.jl
@@ -1,3 +1,11 @@
+#
+# Project : Gardenia
+# Source  : global.jl
+# Author  : Li Huang (huangli@caep.cn)
+# Status  : Unstable
+#
+# Last modified: 2022/01/29
+#
 
 const I64 = Int64
 const F64 = Float64
