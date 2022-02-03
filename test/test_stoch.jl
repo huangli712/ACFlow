802cf6f6e3c586b818655da247ddaa60afe678c1
Li Huang <lihuang.dmft@gmail.com>
2022-02-03 16:47:32 +0800

    update test

--- test/test_stoch.jl
+++ test/test_stoch.jl
@@ -5,7 +5,7 @@ push!(LOAD_PATH, "/Users/lihuang/Working/devel/acflow/src")
 using ACFlow
 
 println("Stochastic Analytical Continuation")
-G, tmesh = StochAC.read_data()
-MC, SE, SG, SC = StochAC.stoch_init(tmesh, G)
+Gtau, Gdev, tmesh = StochAC.read_data()
+MC, SE, SC = StochAC.stoch_init(tmesh, Gtau, Gdev)
 
-StochAC.stoch_run(MC, SE, SC, SG, G)
+StochAC.stoch_run(MC, SE, SC)
