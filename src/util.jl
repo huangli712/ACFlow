77997f219cf128ee88777cdc293213c0ea578db1
Li Huang <lihuang.dmft@gmail.com>
2022-02-05 11:58:15 +0800

    add more functions in util.jl

--- src/util.jl
+++ src/util.jl
@@ -158,6 +158,17 @@ function query_args()
     end
 end
 
+#=
+### *Colorful Outputs*
+=#
+
+
+
+
+
+
+
+
 @inline function line_to_array(io::IOStream)
     split(readline(io), " ", keepempty = false)
 end
@@ -321,4 +332,140 @@ function simpson(x::Vector{F64}, y::Vector{T}) where {T}
     val = h / 3.0 * (y[1] + y[end] + 2.0 * even_sum + 4.0 * odd_sum)
 
     return val
+end
+
+#=
+### *Color Tools*
+=#
+
+#=
+*Remarks* :
+
+The purpose of the following codes is to provide some convenient tools
+to output colorful and stylized texts in the terminal. Actually, these
+codes are inspried by this repository:
+
+* https://github.com/Aerlinger/AnsiColor.jl
+
+For more information about the ANSI color escape sequences, please check
+the following websites further:
+
+* https://stackoverflow.com/questions/4842424/
+* https://en.wikipedia.org/wiki/ANSI_escape_code
+
+Note that the macro `@pcs` and functions `prompt()` rely on these codes.
+=#
+
+"""
+    COLORS
+
+A global dict, which is used to specify the system colors.
+"""
+const COLORS = Dict{String,I64}(
+    "black"          => 0,
+    "red"            => 1,
+    "green"          => 2,
+    "yellow"         => 3,
+    "blue"           => 4,
+    "magenta"        => 5,
+    "cyan"           => 6,
+    "white"          => 7,
+    "default"        => 9,
+    "light_black"    => 60,
+    "light_red"      => 61,
+    "light_green"    => 62,
+    "light_yellow"   => 63,
+    "light_blue"     => 64,
+    "light_magenta"  => 65,
+    "light_cyan"     => 66,
+    "light_white"    => 67
+)
+
+"""
+    MODES
+
+A global dict, which is used to specify the mode for output characters.
+"""
+const MODES = Dict{String,I64}(
+    "default"        => 0,
+    "bold"           => 1,
+    "underline"      => 4,
+    "blink"          => 5,
+    "swap"           => 7,
+    "hide"           => 8
+)
+
+"""
+    colorize(c::String, s::String; bg::String = "default", m::String="default")
+
+Return some escape sequences, which will be displayed as colorized texts
+in the terminal.
+"""
+function colorize(c::String, s::String; bg::String = "default", m::String="default")
+    C_OFFSET = 30
+    B_OFFSET = 40
+    "\033[$(MODES[m]);$(C_OFFSET + COLORS[c]);$(B_OFFSET + COLORS[bg])m$(s)\033[0m"
+end
+
+"""
+    colorize(c::String, s::String; bg::String = "default", m::String="default")
+
+Return some escape sequences, which will be displayed as colorized texts
+in the terminal.
+"""
+function colorize(c::Symbol, s::String; bg::String = "default", m::String="default")
+    colorize(string(c), s; bg=bg, m=m)
+end
+
+#=
+*Remarks* :
+
+The following codes will generate and export dynamically some color
+functions, including:
+
+```julia
+# For standard colors
+black(str::String)
+red(str::String)
+green(str::String)
+yellow(str::String)
+blue(str::String)
+magenta(str::String)
+cyan(str::String)
+white(str::String)
+```
+
+and their light color versions
+
+```julia
+# For light colors
+light_black(str::String)
+light_red(str::String)
+light_green(str::String)
+light_yellow(str::String)
+light_blue(str::String)
+light_magenta(str::String)
+light_cyan(str::String)
+light_white(str::String)
+```
+
+These functions provide some shortcuts to create texts decorated by
+special escape sequences. These texts will be show as colorized texts
+in the terminal.
+
+### Examples
+```julia-repl
+julia> println(red("hello world!"))
+```
+=#
+
+export COLORS
+export MODES
+export colorize
+
+for k in keys(COLORS)
+    f = Symbol(k)
+    k == "default" && continue
+    @eval ($f)(str::String) = colorize(Symbol($f), str)
+    @eval export ($f)
 end
