a6ee56d2daffed18e41a270906ea80cddf3cfac5
Li Huang <lihuang.dmft@gmail.com>
2022-04-10 00:26:15 +0800

    fix curve_fit.jl

--- test/test1/curve_fit.jl
+++ test/test1/curve_fit.jl
@@ -1,44 +1,7 @@
-abstract type AbstractObjective end
-
-# Initialize an n-by-n Jacobian
-alloc_DF(x, F) = eltype(x)(NaN) .* vec(F) .* vec(x)'
-
 x_of_nans(x, Tf=eltype(x)) = fill!(Tf.(x), Tf(NaN))
 
-function f!_from_f(f, F::AbstractArray, inplace)
-    if inplace
-        return f
-    else
-        return function ff!(F, x)
-            copyto!(F, f(x))
-            F
-        end
-    end
-end
-function df!_from_df(j, F::AbstractArray, inplace)
-    if inplace
-        return j
-    else
-        return function jj!(J, x)
-            copyto!(J, j(x))
-            J
-        end
-    end
-end
-function fdf!_from_fdf(fj, F::AbstractArray, inplace)
-    if inplace
-        return fj
-    else
-        return function ffjj!(F, J, x)
-            fx, jx = fj(x)
-            copyto!(J, jx)
-            copyto!(F, fx)
-        end
-    end
-end
-
 # Used for objectives and solvers where the gradient is available/exists
-mutable struct OnceDifferentiable{TF, TDF, TX} <: AbstractObjective
+mutable struct OnceDifferentiable{TF, TDF, TX}
     f # objective
     df # (partial) derivative of objective
     fdf # objective and (partial) derivative of objective
@@ -50,63 +13,63 @@ mutable struct OnceDifferentiable{TF, TDF, TX} <: AbstractObjective
     df_calls::Vector{Int}
 end
 
-### Only the objective
-function OnceDifferentiable(f, x::AbstractArray, F::AbstractArray, DF::AbstractArray = alloc_DF(x, F); inplace = true, autodiff = :finite)
-    f! = f!_from_f(f, F, inplace)
-    OnceDifferentiable(f!, x::AbstractArray, F::AbstractArray, DF, autodiff)
-end
-
-function OnceDifferentiable(f, x_seed::AbstractArray, F::AbstractArray, DF::AbstractArray, autodiff::Symbol)
-    # Figure out which Val-type to use for FiniteDiff based on our symbol interface.
-    fdtype = Val{:central}
-    # Apparently only the third input is aliased.
-    j_finitediff_cache = FiniteDiff.JacobianCache(copy(x_seed), copy(F), copy(F), fdtype)
-
-    function fj_finitediff!(F, J, x)
-        f(F, x)
-        FiniteDiff.finite_difference_jacobian!(J, f, x, j_finitediff_cache)
+function OnceDifferentiable(f, x_seed::AbstractArray, F::AbstractArray)
+    function f!(F, x)
+        copyto!(F, f(x))
         F
     end
+
+    function fj_finitediff!(F, J, x)
+        f!(F, x)
+        FiniteDiff.finite_difference_jacobian!(J, f!, x, j_finitediff_cache)
+        F
+    end
+
     function j_finitediff!(J, x)
         F_cache = copy(F)
         fj_finitediff!(F_cache, J, x)
     end
 
-    return OnceDifferentiable(f, j_finitediff!, fj_finitediff!, x_seed, F, DF)
+    DF = eltype(x_seed)(NaN) .* vec(F) .* vec(x_seed)'
+
+    # Figure out which Val-type to use for FiniteDiff based on our symbol interface.
+    fdtype = Val{:central}
+
+    # Apparently only the third input is aliased.
+    j_finitediff_cache = FiniteDiff.JacobianCache(copy(x_seed), copy(F), copy(F), fdtype)
+
+    x_f, x_df = x_of_nans(x_seed), x_of_nans(x_seed)
+    OnceDifferentiable(f!, j_finitediff!, fj_finitediff!, copy(F), copy(DF), x_f, x_df, [0,], [0,])
 end
 
-### Objective and derivative
-function OnceDifferentiable(f, df, fdf, x::AbstractArray, F::AbstractArray, DF::AbstractArray = alloc_DF(x, F); inplace = true)
-    f = f!_from_f(f, F, inplace)
-    df! = df!_from_df(df, F, inplace)
-    fdf! = fdf!_from_fdf(fdf, F, inplace)
-    x_f, x_df = x_of_nans(x), x_of_nans(x)
-    OnceDifferentiable(f, df!, fdf!, copy(F), copy(DF), x_f, x_df, [0,], [0,])
-end
-
-abstract type Optimizer end
-struct LevenbergMarquardt <: Optimizer end
-struct Avv
-        h!::Function
-    hessians::Array{Float64}
-
-    function Avv(h!::Function, n::Int, m::Int)
-        hessians = Array{Float64}(undef, m*n, n)
-        new(h!, hessians)
-    end
-end
-value(obj::AbstractObjective) = obj.F
-function value(obj::AbstractObjective, F, x)
+value(obj::OnceDifferentiable) = obj.F
+function value(obj::OnceDifferentiable, F, x)
     obj.f_calls .+= 1
     return obj.f(F, x)
 end
-function value!(obj::AbstractObjective, x)
+function value!(obj::OnceDifferentiable, x)
     if x != obj.x_f
         value!!(obj, x)
     end
     value(obj)
 end
-jacobian(obj::AbstractObjective) = obj.DF
+
+jacobian(obj::OnceDifferentiable) = obj.DF
+function jacobian!(obj, x)
+    if x != obj.x_df
+        jacobian!!(obj, x)
+    end
+    jacobian(obj)
+end
+jacobian!!(obj, x) = jacobian!!(obj, obj.DF, x)
+function jacobian!!(obj, J, x)
+    obj.df(J, x)
+    copyto!(obj.x_df, x)
+    obj.df_calls .+= 1
+    obj.df_calls
+    J
+end
+
 value_jacobian!!(obj, x) = value_jacobian!!(obj, obj.F, obj.DF, x)
 function value_jacobian!!(obj, F, J, x)
     obj.fdf(F, J, x)
@@ -118,92 +81,30 @@ function value_jacobian!!(obj, F, J, x)
     F, J
 end
 
-
-function jacobian!(obj, x)
-    if x != obj.x_df
-        jacobian!!(obj, x)
-    end
-    jacobian(obj)
-end
-
-jacobian!!(obj, x) = jacobian!!(obj, obj.DF, x)
-function jacobian!!(obj, J, x)
-    obj.df(J, x)
-    copyto!(obj.x_df, x)
-    obj.df_calls .+= 1
-    obj.df_calls
-    J
-end
-
-struct OptimizationState{T <: Optimizer}
-    iteration::Int
-    value::Float64
-    g_norm::Float64
-    metadata::Dict
-end
-#=
-function Base.show(io::IO, t::OptimizationState)
-    @printf io "%6d   %14e   %14e\n" t.iteration t.value t.g_norm
-    if !isempty(t.metadata)
-        for (key, value) in t.metadata
-            @printf io " * %s: %s\n" key value
-        end
-    end
-    return
-end
-=#
-
-OptimizationTrace{T} = Vector{OptimizationState{T}}
-#=
-function Base.show(io::IO, tr::OptimizationTrace)
-    @printf io "Iter     Function value   Gradient norm \n"
-    @printf io "------   --------------   --------------\n"
-    for state in tr
-        show(io, state)
-    end
-    return
-end
-=#
-
-abstract type OptimizationResults end
-minimizer(r::OptimizationResults) = r.minimizer
-mutable struct MultivariateOptimizationResults{O<:Optimizer,T,N} <: OptimizationResults
-    method::O
+mutable struct OptimizationResults{T,N}
     initial_x::Array{T,N}
     minimizer::Array{T,N}
     minimum::T
     iterations::Int
     iteration_converged::Bool
     x_converged::Bool
-    x_tol::Float64
-    x_residual::Float64
     f_converged::Bool
-    f_tol::Float64
-    f_residual::Float64
     g_converged::Bool
-    g_tol::Float64
-    g_residual::Float64
     f_increased::Bool
-    trace::OptimizationTrace{O}
     f_calls::Int
     g_calls::Int
-    h_calls::Int
 end
-converged(r::MultivariateOptimizationResults) = r.x_converged || r.f_converged || r.g_converged
+minimizer(r::OptimizationResults) = r.minimizer
+converged(r::OptimizationResults) = r.x_converged || r.f_converged || r.g_converged
 
-Base.summary(::LevenbergMarquardt) = "Levenberg-Marquardt"
 """
-    `levenberg_marquardt(f, g, initial_x; <keyword arguments>`
+    levenberg_marquardt(f, initial_x; kwargs...)
 
 Returns the argmin over x of `sum(f(x).^2)` using the Levenberg-Marquardt
 algorithm, and an estimate of the Jacobian of `f` at x.
 
 The function `f` should take an input vector of length n and return an output
-vector of length m. The function `g` is the Jacobian of f, and should return an m x
-n matrix. `initial_x` is an initial guess for the solution.
-
-Implements box constraints as described in Kanzow, Yamashita, Fukushima (2004; J
-Comp & Applied Math).
+vector of length m. `initial_x` is an initial guess for the solution.
 
 # Keyword arguments
 * `x_tol::Real=1e-8`: search tolerance in x
@@ -215,20 +116,11 @@ Comp & Applied Math).
 * `tau=Inf`: set initial trust region radius using the heuristic : tau*maximum(jacobian(df)'*jacobian(df))
 * `lambda_increase=10.0`: `lambda` is multiplied by this factor after step below min quality
 * `lambda_decrease=0.1`: `lambda` is multiplied by this factor after good quality steps
-* `show_trace::Bool=false`: print a status summary on each iteration if true
-* `lower,upper=[]`: bound solution to these limits
 """
-
-# I think a smarter way to do this *might* be to create a type similar to `OnceDifferentiable`
-# and the like. This way we could not only merge the two functions, but also have a convinient
-# way to provide an autodiff-made acceleration when someone doesn't provide an `avv`.
-# it would probably be very inefficient performace-wise for most cases, but it wouldn't hurt to have it somewhere
 function levenberg_marquardt(df::OnceDifferentiable, initial_x::AbstractVector{T};
     x_tol::Real = 1e-8, g_tol::Real = 1e-12, maxIter::Integer = 1000,
     lambda = T(10), tau=T(Inf), lambda_increase::Real = 10.0, lambda_decrease::Real = 0.1,
-    min_step_quality::Real = 1e-3, good_step_quality::Real = 0.75,
-    show_trace::Bool = false, lower::Vector{T} = Array{T}(undef, 0), upper::Vector{T} = Array{T}(undef, 0), avv!::Union{Function,Nothing,Avv} = nothing
-    ) where T
+    min_step_quality::Real = 1e-3, good_step_quality::Real = 0.75) where T
 
     # First evaluation
     value_jacobian!!(df, initial_x)
@@ -237,22 +129,15 @@ function levenberg_marquardt(df::OnceDifferentiable, initial_x::AbstractVector{T
         lambda = tau*maximum(jacobian(df)'*jacobian(df))
     end
 
-
     # check parameters
-    ((isempty(lower) || length(lower)==length(initial_x)) && (isempty(upper) || length(upper)==length(initial_x))) ||
-            throw(ArgumentError("Bounds must either be empty or of the same length as the number of parameters."))
-    ((isempty(lower) || all(initial_x .>= lower)) && (isempty(upper) || all(initial_x .<= upper))) ||
-            throw(ArgumentError("Initial guess must be within bounds."))
     (0 <= min_step_quality < 1) || throw(ArgumentError(" 0 <= min_step_quality < 1 must hold."))
     (0 < good_step_quality <= 1) || throw(ArgumentError(" 0 < good_step_quality <= 1 must hold."))
     (min_step_quality < good_step_quality) || throw(ArgumentError("min_step_quality < good_step_quality must hold."))
-
 
     # other constants
     MAX_LAMBDA = 1e16 # minimum trust region radius
     MIN_LAMBDA = 1e-16 # maximum trust region radius
     MIN_DIAGONAL = 1e-6 # lower bound on values of diagonal matrix used to regularize the trust region step
-
 
     converged = false
     x_converged = false
@@ -276,15 +161,6 @@ function levenberg_marquardt(df::OnceDifferentiable, initial_x::AbstractVector{T
     J = jacobian(df)
     dir_deriv = Array{T}(undef,m)
     v = Array{T}(undef,n)
-
-    # Maintain a trace of the system.
-    tr = OptimizationTrace{LevenbergMarquardt}()
-    if show_trace
-        d = Dict("lambda" => lambda)
-        os = OptimizationState{LevenbergMarquardt}(iterCt, sum(abs2, value(df)), NaN, d)
-        push!(tr, os)
-        println(os)
-    end
 
     while (~converged && iterCt < maxIter)
         # jacobian! will check if x is new or not, so it is only actually
@@ -317,36 +193,7 @@ function levenberg_marquardt(df::OnceDifferentiable, initial_x::AbstractVector{T
 
         v .= JJ \ n_buffer
 
-
-        if avv! != nothing
-            #GEODESIC ACCELERATION PART
-            avv!(dir_deriv, x, v)
-            mul!(a, transpose(J), dir_deriv)
-            rmul!(a, -1) #we multiply by -1 before the decomposition/division
-            LAPACK.potrf!('U', JJ) #in place cholesky decomposition
-            LAPACK.potrs!('U', JJ, a) #divides a by JJ, taking into account the fact that JJ is now the `U` cholesky decoposition of what it was before
-            rmul!(a, 0.5)
-            delta_x .= v .+ a
-            #end of the GEODESIC ACCELERATION PART
-        else
-            delta_x = v
-        end
-
-
-
-
-
-        # apply box constraints
-        if !isempty(lower)
-            @simd for i in 1:n
-               @inbounds delta_x[i] = max(x[i] + delta_x[i], lower[i]) - x[i]
-            end
-        end
-        if !isempty(upper)
-            @simd for i in 1:n
-               @inbounds delta_x[i] = min(x[i] + delta_x[i], upper[i]) - x[i]
-            end
-        end
+        delta_x = v
 
         # if the linear assumption is valid, our new residual should be:
         mul!(Jdelta_buffer, J, delta_x)
@@ -386,15 +233,6 @@ function levenberg_marquardt(df::OnceDifferentiable, initial_x::AbstractVector{T
 
         iterCt += 1
 
-        # show state
-        if show_trace
-            g_norm = norm(J' * value(df), Inf)
-            d = Dict("g(x)" => g_norm, "dx" => delta_x, "lambda" => lambda)
-            os = OptimizationState{LevenbergMarquardt}(iterCt, sum(abs2, value(df)), g_norm, d)
-            push!(tr, os)
-            println(os)
-        end
-
         # check convergence criteria:
         # 1. Small gradient: norm(J^T * value(df), Inf) < g_tol
         # 2. Small step size: norm(delta_x) < x_tol
@@ -407,105 +245,39 @@ function levenberg_marquardt(df::OnceDifferentiable, initial_x::AbstractVector{T
         converged = g_converged | x_converged
     end
 
-    MultivariateOptimizationResults(
-        LevenbergMarquardt(),    # method
+    OptimizationResults(
         initial_x,             # initial_x
         x,                     # minimizer
-        sum(abs2, value(df)),       # minimum
+        sum(abs2, value(df)),  # minimum
         iterCt,                # iterations
         !converged,            # iteration_converged
         x_converged,           # x_converged
-        0.0,                   # x_tol
-        0.0,
         false,                 # f_converged
-        0.0,                   # f_tol
-        0.0,
         g_converged,           # g_converged
-        g_tol,                  # g_tol
-        0.0,
         false,                 # f_increased
-        tr,                    # trace
-        first(df.f_calls),               # f_calls
-        first(df.df_calls),               # g_calls
-        0                      # h_calls
+        first(df.f_calls),     # f_calls
+        first(df.df_calls),    # g_calls
     )
 end
 
-struct LsqFitResult{P, R, J, W <: AbstractArray}
+struct LsqFitResult{P, R, J}
     param::P
     resid::R
     jacobian::J
     converged::Bool
-    wt::W
 end
 
-function check_data_health(xdata, ydata)
-    if any(ismissing, xdata) || any(ismissing, ydata)
-        error("Data contains `missing` values and a fit cannot be performed")
-    end
-    if any(isinf, xdata) || any(isinf, ydata) || any(isnan, xdata) || any(isnan, ydata)
-        error("Data contains `Inf` or `NaN` values and a fit cannot be performed")
-    end
-end
+"""
+    curve_fit(model, x, y, p0)
 
-function lmfit(f, p0::AbstractArray, wt::AbstractArray; autodiff = :finite, kwargs...)
-    # this is a convenience function for the curve_fit() methods
-    # which assume f(p) is the cost functionj i.e. the residual of a
-    # model where
-    #   model(xpts, params...) = ydata + error (noise)
-
-    # this minimizes f(p) using a least squares sum of squared error:
-    #   rss = sum(f(p)^2)
-    #
-    # returns p, f(p), g(p) where
-    #   p    : best fit parameters
-    #   f(p) : function evaluated at best fit p, (weighted) residuals
-    #   g(p) : estimated Jacobian at p (Jacobian with respect to p)
-
-    # construct Jacobian function, which uses finite difference method
+Fit data to a non-linear `model`. `p0` is an initial model parameter guess.
+The return object is a composite type (`LsqFitResult`).
+"""
+function curve_fit(model, x::AbstractArray, y::AbstractArray, p0::AbstractArray)
+    f = (p) -> model(x, p) - y
     r = f(p0)
-    autodiff = autodiff == :forwarddiff ? :forward : autodiff
-    R = OnceDifferentiable(f, p0, copy(r); inplace = false, autodiff = autodiff)
-    results = levenberg_marquardt(R, p0; kwargs...)
+    R = OnceDifferentiable(f, p0, copy(r))
+    results = levenberg_marquardt(R, p0)
     p = minimizer(results)
-    return LsqFitResult(p, value!(R, p), jacobian!(R, p), converged(results), wt)
-end
-
-"""
-    curve_fit(model, xdata, ydata, p0) -> fit
-Fit data to a non-linear `model`. `p0` is an initial model parameter guess (see Example).
-The return object is a composite type (`LsqFitResult`), with some interesting values:
-
-* `fit.resid` : residuals = vector of residuals
-* `fit.jacobian` : estimated Jacobian at solution
-
-additionally, it is possible to quiry the degrees of freedom with
-
-* `dof(fit)`
-* `coef(fit)`
-
-## Example
-```julia
-# a two-parameter exponential model
-# x: array of independent variables
-# p: array of model parameters
-model(x, p) = p[1]*exp.(-x.*p[2])
-
-# some example data
-# xdata: independent variables
-# ydata: dependent variable
-xdata = range(0, stop=10, length=20)
-ydata = model(xdata, [1.0 2.0]) + 0.01*randn(length(xdata))
-p0 = [0.5, 0.5]
-
-fit = curve_fit(model, xdata, ydata, p0)
-```
-"""
-function curve_fit(model, xdata::AbstractArray, ydata::AbstractArray, p0::AbstractArray; kwargs...)
-    check_data_health(xdata, ydata)
-    # construct the cost function
-    T = eltype(ydata)
-
-    f = (p) -> model(xdata, p) - ydata
-    lmfit(f, p0, T[]; kwargs...)
+    return LsqFitResult(p, value!(R, p), jacobian!(R, p), converged(results))
 end
