482832d023d1e652e2a0946c1a5c3123cfd455c4
Li Huang <lihuang.dmft@gmail.com>
2022-04-21 00:09:29 +0800

    fix version number

--- src/global.jl
+++ src/global.jl
@@ -121,7 +121,7 @@ Version of this julia package.
 
 See also: [`__RELEASE__`](@ref).
 """
-const __VERSION__ = v"0.5.3-devel.220418"
+const __VERSION__ = v"0.5.4-devel.220421"
 
 """
     __RELEASE__
