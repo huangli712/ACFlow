1840acfe9795172e1574e773f5eebdd40a65f58b
Li Huang <lihuang.dmft@gmail.com>
2022-04-25 00:03:45 +0800

    fix som.jl

--- src/global.jl
+++ src/global.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2022/04/18
+# Last modified: 2022/04/25
 #
 
 #=
@@ -121,7 +121,7 @@ Version of this julia package.
 
 See also: [`__RELEASE__`](@ref).
 """
-const __VERSION__ = v"0.5.6-devel.220424"
+const __VERSION__ = v"0.5.7-devel.220425"
 
 """
     __RELEASE__
--- src/som.jl
+++ src/som.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2022/04/24
+# Last modified: 2022/04/25
 #
 
 #=
