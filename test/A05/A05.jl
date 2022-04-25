93d515dda35cd75067f3a3473026536a71c7087c
Li Huang <lihuang.dmft@gmail.com>
2022-04-25 10:18:53 +0800

    fix A05.jl

--- test/A05/A05.jl
+++ test/A05/A05.jl
@@ -6,7 +6,7 @@ using ACFlow
 
 welcome()
 
-
+# For MaxEnt solver
 
 # Setup parameters
 C = Dict{String,Any}(
@@ -29,3 +29,28 @@ setup_param(C, S)
 
 # Call the solver
 Aout, Gout = solve(read_data())
+
+# Backup calculated results
+cp("Aout.data", "Aout.mem.data", force = true)
+cp("Gout.data", "Gout.mem.data", force = true)
+cp("repr.data", "repr.mem.data", force = true)
+
+# For StochOM solver
+
+# Setup parameters
+C = Dict{String,Any}(
+    "solver" => "StochOM"
+)
+#
+S = Dict{String,Any}(
+)
+#
+setup_param(C, S, false)
+
+# Call the solver
+Aout, Gout = solve(read_data())
+
+# Backup calculated results
+cp("Aout.data", "Aout.som.data", force = true)
+cp("Gout.data", "Gout.som.data", force = true)
+cp("repr.data", "repr.som.data", force = true)
