828b30669d7d3f261d8d6ba8110f6627130d558e
Li Huang <lihuang.dmft@gmail.com>
2022-04-28 11:15:16 +0800

    fix version number

--- src/global.jl
+++ src/global.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2022/04/27
+# Last modified: 2022/04/28
 #
 
 #=
@@ -121,7 +121,7 @@ Version of this julia package.
 
 See also: [`__RELEASE__`](@ref).
 """
-const __VERSION__ = v"0.6.0-devel.220428"
+const __VERSION__ = v"0.6.1-devel.220428"
 
 """
     __RELEASE__
