abb2b3cf3ddc68a3893208dbd6da241545d13250
Li Huang <lihuang.dmft@gmail.com>
2022-04-29 10:26:55 +0800

    fix comments

--- src/model.jl
+++ src/model.jl
@@ -4,15 +4,18 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2022/04/27
+# Last modified: 2022/04/29
 #
 
 #=
 *Remarks* : Default Models
 
-Now `ACFlow` supports the following default model functions. Note that
-in principle, the `StochOM` solver (stochastic optimization method) does
-not need the default model functions.
+Now `ACFlow` supports various default model functions `m(ω)`. Note that
+the `StochOM` solver (stochastic optimization method) does not need the
+default model functions, and the `StochAC` solver (stochastic analytical
+continuation method) only need the `flat` default model function.
+
+These default model functions are summaried as follows.
 
 Flat model (`flat`):
 
@@ -88,12 +91,17 @@ Rise-And-Decay model (`risedecay`):
 m(ω) = Γ\omega^2 \exp{(-\Gamma\omega)},\quad \omega \ge 0
 \end{equation}
 ```
+
+Be careful, ``\Gamma``, ``\omega_1``, and ``\omega_2`` are all parameters.
+Their default values are defined in `make_model()`, but you can modified
+them according to `pmodel`.
 =#
 
 """
     build_flat_model(am::AbstractMesh)
 
 Try to build a flat model in `am`. Then this model function is normalized.
+Only this model function is suitable for the `StochAC` solver.
 
 See also: [`AbstractMesh`](@ref).
 """
@@ -124,7 +132,8 @@ end
 
 Try to build a shifted Gaussian model, which is then normalized. The
 argument `Γ` is used to control the width of the Gaussian peak, and `s`
-means the shift of the central peak.
+means the shift of the central peak. If `s > 0`, the peak is shifted to
+positive half-axis, and vice versa.
 
 See also: [`AbstractMesh`](@ref).
 """
@@ -138,7 +147,7 @@ end
 """
     build_2gaussians_model(am::AbstractMesh, Γ::F64, s1::F64, s2::F64)
 
-Try to build a Two-Gaussians model, which is then normalized. The
+Try to build a Two Gaussians model, which is then normalized. The
 argument `Γ` is used to control the width of the Gaussian peak, and
 `s1` and `s2` denote the centers of the two peaks.
 
@@ -173,7 +182,8 @@ end
 
 Try to build a shifted Lorentzian model, which is then normalized. The
 argument `Γ` is used to control the width of the Lorentzian peak, and `s`
-means the shift of the central peak.
+means the shift of the central peak. If `s > 0`, the peak is shifted to
+positive half-axis, and vice versa.
 
 See also: [`AbstractMesh`](@ref).
 """
@@ -205,7 +215,9 @@ end
 """
     build_risedecay_model(am::AbstractMesh, Γ::F64)
 
-Try to build a Rise-And-Decay model, which is then normalized.
+Try to build a Rise-And-Decay model, which is then normalized. This model
+function is defined on positive half-axis, so it is more suitable for the
+bosonic response function.
 
 See also: [`AbstractMesh`](@ref).
 """
@@ -243,7 +255,8 @@ end
     build_func_model(fun::Function, am::AbstractMesh, kwargs...)
 
 Try to build a model function by customized function `fun`. `kwargs`
-denotes the arguments required by `fun`.
+denotes the arguments required by `fun`. Actually, this feature does
+not really work.
 
 See also: [`AbstractMesh`](@ref).
 """
