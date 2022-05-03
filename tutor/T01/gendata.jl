87637cce806156a9fce3fd16aee7c360f13727fb
Li Huang <lihuang.dmft@gmail.com>
2022-05-03 09:19:17 +0800

    fix ac.toml and gendata.jl

--- tutor/T01/gendata.jl
+++ tutor/T01/gendata.jl
@@ -3,7 +3,10 @@
 using DelimitedFiles
 using Printf
 
+# Number of grid points for input data
 ngrid = 100
+
+# Read Matsubara green's function from solver.grn.dat
 dlm = readdlm("solver.grn.dat")
 grid = dlm[1:ngrid,2]
 grn  = dlm[1:ngrid,3] + im * dlm[1:ngrid,4]
@@ -16,5 +19,21 @@ open("green.data", "w") do fout
         ze = err[i]
         @printf(fout, "%20.16f %20.16f %20.16f %20.16f %20.16f\n",
             grid[i], real(zg), imag(zg), real(ze), imag(ze))
+    end
+end
+
+# Read Matsubara self-energy function from solver.sgm.dat
+dlm = readdlm("solver.sgm.dat")
+grid = dlm[1:ngrid,2]
+sgm  = dlm[1:ngrid,3] + im * dlm[1:ngrid,4]
+err  = dlm[1:ngrid,5] + im * dlm[1:ngrid,6]
+
+# Write self-energy function
+open("sigma.data", "w") do fout
+    for i in eachindex(grid)
+        zs = sgm[i]
+        ze = err[i]
+        @printf(fout, "%20.16f %20.16f %20.16f %20.16f %20.16f\n",
+            grid[i], real(zs), imag(zs), real(ze), imag(ze))
     end
 end
