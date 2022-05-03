c6f7d9f4b0f23a966c2962b8093460955762b227
Li Huang <lihuang.dmft@gmail.com>
2022-05-03 11:29:18 +0800

    add gendata.jl

---
+++ tutor/T03/gendata.jl
@@ -0,0 +1,81 @@
+#!/usr/bin/env julia
+
+using Random
+using Printf
+
+# Numerical integration by the composite trapezoidal rule
+function trapz(x, y, linear::Bool = false)
+    # For linear mesh
+    if linear
+        h = x[2] - x[1]
+        value = y[1] + y[end] + 2.0 * sum(y[2:end-1])
+        value = h * value / 2.0
+    # For non-equidistant mesh
+    else
+        len = length(x)
+        value = 0.0
+        for i = 1:len-1
+            value = value + (y[i] + y[i+1]) * (x[i+1] - x[i])
+        end
+        value = value / 2.0
+    end
+
+    return value
+end
+
+# Setup parameters
+wmin = +0.0 # Left boundary
+wmax = +8.0 # Right boundary
+nmesh = 801 # Number of real-frequency points
+niw  = 20   # Number of Matsubara frequencies
+beta = 20.0 # Inverse temperature
+
+# Real frequency mesh
+w_real = collect(LinRange(wmin, wmax, nmesh))
+
+# Spectral function
+spec_real = similar(w_real)
+@. spec_real = exp(-(w_real - 2.0) ^ 2.0 / 2.0) +
+               exp(-(w_real + 2.0) ^ 2.0 / 2.0)
+spec_real = spec_real ./ trapz(w_real, spec_real)
+
+# Matsubara frequency mesh
+iw = 2.0 * π / beta * collect(0:niw-1)
+
+# Noise
+seed = rand(1:100000000)
+rng = MersenneTwister(seed)
+noise_amplitude = 1.0e-4
+noise = randn(rng, Float64, niw) * noise_amplitude
+
+# Kernel function
+kernel = reshape(w_real .^ 2.0, (1,nmesh)) ./
+         (reshape(iw .^ 2.0, (niw,1)) .+ reshape(w_real .^ 2.0, (1,nmesh)))
+kernel[1,1] = 1.0
+
+# Build green's function
+KA = kernel .* reshape(spec_real, (1,nmesh))
+gf_mats = zeros(Float64, niw)
+for i in eachindex(gf_mats)
+    gf_mats[i] = trapz(w_real, KA[i,:]) + noise[i]
+end
+norm = gf_mats[1]
+gf_mats = gf_mats / norm
+
+# Build error
+err = ones(Float64, niw) * noise_amplitude
+
+# Write green's function
+open("chi.data", "w") do fout
+    for i in eachindex(gf_mats)
+        z = gf_mats[i]
+        @printf(fout, "%20.16f %20.16f %20.16f\n", iw[i], z, err[i])
+    end
+end
+
+# Write spectral function
+open("exact.data", "w") do fout
+    for i in eachindex(spec_real)
+        @printf(fout, "%20.16f %20.16f\n", w_real[i], spec_real[i])
+    end
+end
