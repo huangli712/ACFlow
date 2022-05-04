fb3b9bec8af339468b5d40959a16ff827b453850
Li Huang <lihuang.dmft@gmail.com>
2022-05-05 00:08:25 +0800

    fix A01

--- test/A01/gendata.jl
+++ test/A01/gendata.jl
@@ -1,27 +1,10 @@
 #!/usr/bin/env julia
 
+push!(LOAD_PATH, ENV["ACFLOW_HOME"])
+
 using Random
 using Printf
-
-# Numerical integration by the composite trapezoidal rule
-function trapz(x, y, linear::Bool = false)
-    # For linear mesh
-    if linear
-        h = x[2] - x[1]
-        value = y[1] + y[end] + 2.0 * sum(y[2:end-1])
-        value = h * value / 2.0
-    # For non-equidistant mesh
-    else
-        len = length(x)
-        value = 0.0
-        for i = 1:len-1
-            value = value + (y[i] + y[i+1]) * (x[i+1] - x[i])
-        end
-        value = value / 2.0
-    end
-
-    return value
-end
+using ACFlow
 
 # Setup parameters
 wmin = -5.0  # Left boundary
@@ -54,7 +37,7 @@ iw = π / beta * (2.0 * collect(0:niw-1) .+ 1.0)
 seed = rand(1:100000000)
 rng = MersenneTwister(seed)
 noise_ampl = 1.0e-4
-noise_abs = randn(rng, Float64, niw) * noise_ampl
+noise_abs = randn(rng, F64, niw) * noise_ampl
 noise_phase = rand(rng, niw) * 2.0 * π
 noise = noise_abs .* exp.(noise_phase * im)
 
@@ -63,16 +46,16 @@ kernel = 1.0 ./ (im * reshape(iw, (niw,1)) .- reshape(rmesh, (1,nmesh)))
 
 # Build green's function
 KA = kernel .* reshape(image, (1,nmesh))
-giw = zeros(ComplexF64, niw)
+giw = zeros(C64, niw)
 for i in eachindex(giw)
     giw[i] = trapz(rmesh, KA[i,:]) + noise[i]
 end
 
 # Build error
-err = ones(Float64, niw) * noise_ampl
+err = ones(F64, niw) * noise_ampl
 
 # Write green's function
-open("green.data", "w") do fout
+open("giw.data", "w") do fout
     for i in eachindex(giw)
         z = giw[i]
         @printf(fout, "%20.16f %20.16f %20.16f %20.16f\n", iw[i], real(z), imag(z), err[i])
@@ -80,7 +63,7 @@ open("green.data", "w") do fout
 end
 
 # Write spectral function
-open("exact.data", "w") do fout
+open("image.data", "w") do fout
     for i in eachindex(image)
         @printf(fout, "%20.16f %20.16f\n", rmesh[i], image[i])
     end
