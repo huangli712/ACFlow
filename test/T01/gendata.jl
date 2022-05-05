dbf40b97710ba99c876d71d7f9b40bfffacd0015
Li Huang <lihuang.dmft@gmail.com>
2022-05-05 15:07:10 +0800

    fix gendata.jl

--- test/T01/gendata.jl
+++ test/T01/gendata.jl
@@ -9,13 +9,13 @@ ngrid = 100
 # Read Matsubara green's function from solver.grn.dat
 dlm = readdlm("solver.grn.dat")
 grid = dlm[1:ngrid,2]
-grn  = dlm[1:ngrid,3] + im * dlm[1:ngrid,4]
+giw  = dlm[1:ngrid,3] + im * dlm[1:ngrid,4]
 err  = dlm[1:ngrid,5] + im * dlm[1:ngrid,6]
 
 # Write green's function
-open("green.data", "w") do fout
+open("giw.inp", "w") do fout
     for i in eachindex(grid)
-        zg = grn[i]
+        zg = giw[i]
         ze = err[i]
         @printf(fout, "%20.16f %20.16f %20.16f %20.16f %20.16f\n",
             grid[i], real(zg), imag(zg), real(ze), imag(ze))
@@ -25,13 +25,13 @@ end
 # Read Matsubara self-energy function from solver.sgm.dat
 dlm = readdlm("solver.sgm.dat")
 grid = dlm[1:ngrid,2]
-sgm  = dlm[1:ngrid,3] + im * dlm[1:ngrid,4]
+siw  = dlm[1:ngrid,3] + im * dlm[1:ngrid,4]
 err  = dlm[1:ngrid,5] + im * dlm[1:ngrid,6]
 
 # Write self-energy function
-open("sigma.data", "w") do fout
+open("siw.inp", "w") do fout
     for i in eachindex(grid)
-        zs = sgm[i]
+        zs = siw[i]
         ze = err[i]
         @printf(fout, "%20.16f %20.16f %20.16f %20.16f %20.16f\n",
             grid[i], real(zs), imag(zs), real(ze), imag(ze))
