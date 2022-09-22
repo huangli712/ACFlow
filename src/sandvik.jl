9ba4805bfae2ac99469d64d4f1b912e564659fc8
Li Huang <lihuang.dmft@gmail.com>
2022-09-22 21:02:40 +0800

    fix sandvik.jl

--- src/sandvik.jl
+++ src/sandvik.jl
@@ -7,8 +7,6 @@
 # Last modified: 2022/09/22
 #
 
-
-
 struct GreenData
     value :: Vector{N64}
     error :: Vector{N64}
@@ -21,35 +19,6 @@ end
 
 struct FermionicMatsubaraGrid
     grid :: Vector{F64}
-end
-
-function read_data!(::Type{FermionicMatsubaraGrid})
-    grid  = F64[]
-    value = C64[]
-    error = C64[]
-    covar = F64[]
-
-    niw = 64
-    #
-    open("giw.data", "r") do fin
-        for i = 1:niw
-            arr = parse.(F64, line_to_array(fin))
-            push!(grid, arr[1])
-            push!(value, arr[2] + arr[3] * im)
-        end
-    end
-    #
-    open("err.data", "r") do fin
-        for i = 1:niw
-            arr = parse.(F64, line_to_array(fin))
-            @assert grid[i] == arr[1]
-            push!(error, arr[2] + arr[3] * im)
-            push!(covar, arr[2]^2)
-            push!(covar, arr[3]^2)
-        end
-    end
-
-    return FermionicMatsubaraGrid(grid), GreenData(value, error, covar)
 end
 
 mutable struct SACMonteCarlo
