0537d190abdb7b60ad30512b774dbe312841abf7
Li Huang <lihuang.dmft@gmail.com>
2022-10-17 12:39:57 +0800

    fix sigma.jl

--- test/T01/sigma.jl
+++ test/T01/sigma.jl
@@ -17,8 +17,8 @@ dlm = readdlm("siw.inp")
 grid = dlm[:,1]
 #
 # Get self-energy function
-Σinp = dlm[:,2] + im * dlm[:,3]
-Σerr = dlm[:,4] + im * dlm[:,5]
+Σinp = dlm[:,2] + im * dlm[:,3] # Value part
+Σerr = dlm[:,4] + im * dlm[:,5] # Error part
 #
 # Subtract hartree term
 Σ∞  = 1.0
@@ -31,21 +31,22 @@ grid = dlm[:,1]
 # For [BASE] block
 # See types.jl/_PBASE for default setup
 B = Dict{String,Any}(
-    "mtype"  => "gauss",
-    "mesh"   => "tangent",
-    "ngrid"  => 20,
-    "nmesh"  => 801,
-    "wmax"   => 8.0,
-    "wmin"   => -8.0,
-    "beta"   => 10.0,
+    "solver" => "MaxEnt",  # Choose MaxEnt solver
+    "mtype"  => "gauss",   # Default model function
+    "mesh"   => "tangent", # Mesh for spectral function
+    "ngrid"  => 20,        # Number of grid points for input data
+    "nmesh"  => 801,       # Number of mesh points for output data
+    "wmax"   => 8.0,       # Right boundary of mesh
+    "wmin"   => -8.0,      # Left boundary of mesh
+    "beta"   => 10.0,      # Inverse temperature
 )
 #
 # For [MaxEnt] block
 # See types.jl/_PMaxEnt for default setup
 S = Dict{String,Any}(
-    "nalph"  => 15,
-    "alpha"  => 1e12,
-    "blur"   => -1.0,
+    "nalph"  => 15,        # Number of α parameters
+    "alpha"  => 1e12,      # Starting value of α parameter
+    "blur"   => -1.0,      # Enable preblur or not
 )
 #
 setup_param(B, S)
@@ -58,7 +59,7 @@ mesh, Aout, Σout = solve(grid, Σinp, Σerr)
 # Construct final self-energy function
 @. Σout = Σout + Σ∞
 #
-# Write self-energy function
+# Write self-energy function to sigma.data
 open("sigma.data", "w") do fout
     for i in eachindex(mesh)
         z = Σout[i]
