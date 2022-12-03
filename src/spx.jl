d0356d0c8fa49b717f64dba6ccc2b26012f3cd13
Li Huang <lihuang.dmft@gmail.com>
2022-12-03 15:50:41 +0800

    add support to StochPX

--- src/spx.jl
+++ src/spx.jl
@@ -25,3 +25,50 @@ mutable struct StochPXContext
     mesh  :: AbstractMesh
     Aout  :: Vector{F64}
 end
+
+function solve(S::StochPXSolver, rd::RawData)
+end
+
+function init(S::StochPXSolver, rd::RawData)
+end
+
+function run(MC::StochPXMC, SE::StochPXElement, SC::StochPXContext)
+end
+
+function prun(S::StochPXSolver,
+            p1::Dict{String,Vector{Any}},
+            p2::Dict{String,Vector{Any}},
+            MC::StochPXMC, SE::StochPXElement, SC::StochPXContext)
+end
+
+function average(step::F64, SC::StochPXContext)
+end
+
+function last(SC::StochPXContext, Aout::Array{F64,2})
+end
+
+function warmup()
+end
+
+function sample()
+end
+
+function measure()
+end
+
+#=
+### *Service Functions*
+=#
+
+function init_mc(S::StochPXSolver)
+end
+
+function init_element(S::StochPXSolver)
+end
+
+function init_iodata(S::StochPXSolver, rd::RawData)
+end
+
+function constraints(S::StochPXSolver)
+end
+
