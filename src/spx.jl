7a2826cae0e350e82de7fcf9c42896bb5db1b9ad
Li Huang <lihuang.dmft@gmail.com>
2022-12-07 10:26:12 +0800

    fix spx.jl/try_move_a()

--- src/spx.jl
+++ src/spx.jl
@@ -772,61 +772,71 @@ function try_move_a(t::I64, MC::StochPXMC, SE::StochPXElement, SC::StochPXContex
     # It is used to save the change of green's function
     δG = zeros(C64, ngrid)
 
-    # Select two poles randomly
-    s₁ = 1
-    s₂ = 1
-    while s₁ == s₂
-        s₁ = rand(MC.rng, 1:npole)
-        s₂ = rand(MC.rng, 1:npole)
-    end
-
-    # Try to change amplitudes of the two poles, but their sum is kept.
-    P₁ = SE.P[s₁]
-    P₂ = SE.P[s₂]
-    A₁ = SE.A[s₁]
-    A₂ = SE.A[s₂]
-    A₃ = 0.0
-    A₄ = 0.0
-    while true
-        δA = rand(MC.rng) * (A₁ + A₂) - A₁
-        A₃ = A₁ + δA
-        A₄ = A₂ - δA
-
-        if A₃ > 0 && A₄ > 0
-            break
-        end
-    end
-
-    # Calculate change of green's function
+    # Matsubara frequency grid
     iωₙ = im * SC.grid.ω
-    @. δG = (A₃ - A₁) / (iωₙ - SC.fmesh[P₁]) + (A₄ - A₂) / (iωₙ - SC.fmesh[P₂])
 
-    # Calculate new green's function and goodness-of-fit function
-    Gₙ = SC.Gᵧ + vcat(real(δG), imag(δG))
-    χ² = calc_chi2(Gₙ, SC.Gᵥ)
-    δχ² = χ² - SC.χ²[t]
+    # Try to go through each pole
+    for _ = 1:npole
 
-    # Simulated annealing algorithm
-    MC.Atry = MC.Atry + 1
-    if δχ² < 0 || min(1.0, exp(-δχ² * SC.Θ)) > rand(MC.rng)
-        # Update Monte Carlo configuration
-        SE.A[s₁] = A₃
-        SE.A[s₂] = A₄
-
-        # Update reconstructed green's function
-        @. SC.Gᵧ = Gₙ
-
-        # Update goodness-of-fit function
-        SC.χ²[t] = χ²
-
-        # Update Monte Carlo counter
-        MC.Aacc = MC.Aacc + 1
-
-        # Save optimal solution
-        if χ² < SC.χ²min
-            SC.χ²min = χ²
-            measure(t, SE, SC)
+        # Select two poles randomly
+        s₁ = 1
+        s₂ = 1
+        while s₁ == s₂
+            s₁ = rand(MC.rng, 1:npole)
+            s₂ = rand(MC.rng, 1:npole)
         end
+
+        # Try to change amplitudes of the two poles, but their sum is kept.
+        P₁ = SE.P[s₁]
+        P₂ = SE.P[s₂]
+        A₁ = SE.A[s₁]
+        A₂ = SE.A[s₂]
+        A₃ = 0.0
+        A₄ = 0.0
+        while true
+            δA = rand(MC.rng) * (A₁ + A₂) - A₁
+            A₃ = A₁ + δA
+            A₄ = A₂ - δA
+
+            if A₃ > 0 && A₄ > 0
+                break
+            end
+        end
+
+        # Calculate change of green's function
+        @. δG = (
+            +(A₃ - A₁) / (iωₙ - SC.fmesh[P₁])
+            +(A₄ - A₂) / (iωₙ - SC.fmesh[P₂])
+        )
+
+        # Calculate new green's function and goodness-of-fit function
+        Gₙ = SC.Gᵧ + vcat(real(δG), imag(δG))
+        χ² = calc_chi2(Gₙ, SC.Gᵥ)
+        δχ² = χ² - SC.χ²[t]
+
+        # Simulated annealing algorithm
+        MC.Atry = MC.Atry + 1
+        if δχ² < 0 || min(1.0, exp(-δχ² * SC.Θ)) > rand(MC.rng)
+            # Update Monte Carlo configuration
+            SE.A[s₁] = A₃
+            SE.A[s₂] = A₄
+
+            # Update reconstructed green's function
+            @. SC.Gᵧ = Gₙ
+
+            # Update goodness-of-fit function
+            SC.χ²[t] = χ²
+
+            # Update Monte Carlo counter
+            MC.Aacc = MC.Aacc + 1
+
+            # Save optimal solution
+            if χ² < SC.χ²min
+                SC.χ²min = χ²
+                measure(t, SE, SC)
+            end
+        end
+
     end
 end
 
