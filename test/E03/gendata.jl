a93ad14f666a8158bfef3c46e790691abbbd2e2d
Li Huang <lihuang.dmft@gmail.com>
2022-12-10 09:53:38 +0800

    update test

--- test/E03/gendata.jl
+++ test/E03/gendata.jl
@@ -12,34 +12,19 @@ wmax = +5.0  # Right boundary
 nmesh = 2001 # Number of real-frequency points
 niw  = 10    # Number of Matsubara frequencies
 beta = 10.0  # Inverse temperature
-ϵ₁   = 2.00  # Parameters for gaussian peaks
-ϵ₂   = -2.0
-ϵ₃   = 4.00
-ϵ₄   = -4.0
-A₁   = 0.50
-A₂   = 0.50
+ϵ₁   = 4.00  # Parameters for gaussian peaks
+ϵ₂   = -4.0
+ϵ₃   = 1.00
+A₁   = 0.30
+A₂   = 0.30
 A₃   = 0.40
-A₄   = 0.40
-Γ₁   = 0.05
-Γ₂   = 0.05
-Γ₃   = 0.05
-Γ₄   = 0.05
+η    = 1e-2
 
 # Real frequency mesh
-rmesh = collect(LinRange(wmin, wmax, nmesh))
-
-# Spectral function
-image = similar(rmesh)
-#
-@. image  = A₁ * exp(-(rmesh - ϵ₁) ^ 2.0 / (2.0 * Γ₁ ^ 2.0))
-@. image += A₂ * exp(-(rmesh - ϵ₂) ^ 2.0 / (2.0 * Γ₂ ^ 2.0))
-@. image += A₃ * exp(-(rmesh - ϵ₃) ^ 2.0 / (2.0 * Γ₃ ^ 2.0))
-@. image += A₄ * exp(-(rmesh - ϵ₄) ^ 2.0 / (2.0 * Γ₄ ^ 2.0))
-#
-image = image ./ trapz(rmesh, image)
+ω = collect(LinRange(wmin, wmax, nmesh))
 
 # Matsubara frequency mesh
-iw = π / beta * (2.0 * collect(0:niw-1) .+ 1.0)
+iωₙ = π / beta * (2.0 * collect(0:niw-1) .+ 1.0)
 
 # Noise
 seed = rand(1:100000000)
@@ -49,30 +34,32 @@ noise_abs = randn(rng, F64, niw) * noise_ampl
 noise_phase = rand(rng, niw) * 2.0 * π
 noise = noise_abs .* exp.(noise_phase * im)
 
-# Kernel function
-kernel = 1.0 ./ (im * reshape(iw, (niw,1)) .- reshape(rmesh, (1,nmesh)))
-
 # Build green's function
-KA = kernel .* reshape(image, (1,nmesh))
 giw = zeros(C64, niw)
 for i in eachindex(giw)
-    giw[i] = trapz(rmesh, KA[i,:]) + noise[i]
+    giw[i] = A₁ / (iωₙ[i] * im - ϵ₁) + A₂ / (iωₙ[i] * im - ϵ₂) + A₃ / (iωₙ[i] * im - ϵ₃) + noise[i]
+end
+#
+gre = zeros(C64, nmesh)
+for i in eachindex(gre)
+    gre[i] = A₁ / (ω[i] + η * im - ϵ₁) + A₂ / (ω[i] + η * im - ϵ₂) + A₃ / (ω[i] + η * im - ϵ₃)
 end
 
 # Build error
 err = ones(F64, niw) * noise_ampl
 
-# Write green's function
+# Write green's function (Matsubara frequency axis)
 open("giw.data", "w") do fout
     for i in eachindex(giw)
         z = giw[i]
-        @printf(fout, "%20.16f %20.16f %20.16f %20.16f\n", iw[i], real(z), imag(z), err[i])
+        @printf(fout, "%20.16f %20.16f %20.16f %20.16f\n", iωₙ[i], real(z), imag(z), err[i])
     end
 end
 
-# Write spectral function
-open("image.data", "w") do fout
-    for i in eachindex(image)
-        @printf(fout, "%20.16f %20.16f\n", rmesh[i], image[i])
+# Write green's function (real frequency axis)
+open("gre.data", "w") do fout
+    for i in eachindex(gre)
+        z = gre[i]
+        @printf(fout, "%20.16f %20.16f %20.16f\n", ω[i], real(z), imag(z))
     end
 end
