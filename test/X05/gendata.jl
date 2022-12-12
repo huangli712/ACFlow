75a114115adc5c89d5b72af520e58743e821a05d
Li Huang <lihuang.dmft@gmail.com>
2022-12-12 15:56:10 +0800

    update tests

--- test/X05/gendata.jl
+++ test/X05/gendata.jl
@@ -7,114 +7,89 @@ using Printf
 using ACFlow
 
 # Setup parameters
-wmin = +0.0  # Left boundary
-wmax = +8.0  # Right boundary
+wmin = -5.0  # Left boundary
+wmax = +5.0  # Right boundary
 nmesh = 2001 # Number of real-frequency points
 niw  = 10    # Number of Matsubara frequencies
-ntau = 501   # Number of imaginary time points
-beta = 20.0  # Inverse temperature
-W₁   = 0.30  # Parameters for gaussian peaks
-W₂   = 0.20
-Γ₁   = 0.30
-Γ₂   = 1.20
-Γ₃   = 4.00
-ϵ    = 3.00
-
-#
-# For true spectrum
-#
+beta = 10.0  # Inverse temperature
+ϵ₁   = 0.70  # Parameters for δ-like peaks
+ϵ₂   = 1.20
+A₁   = 1.10
+A₂   = 1.335663
+B₁   = A₁ / (2.0 * ϵ₁)
+B₂   = A₂ / (2.0 * ϵ₂)
+η    = 0.05
 
 # Real frequency mesh
-rmesh = collect(LinRange(wmin, wmax, nmesh))
-
-# Spectral function
-image = similar(rmesh)
-#
-for i in eachindex(rmesh)
-    A = W₁ / (1.0 + ((rmesh[i] - 0) / Γ₁) ^ 2.0) +
-        W₂ / (1.0 + ((rmesh[i] - ϵ) / Γ₂) ^ 2.0) +
-        W₂ / (1.0 + ((rmesh[i] + ϵ) / Γ₂) ^ 2.0)
-    image[i] = A / (1.0 + (rmesh[i] / Γ₃) ^ 6.0)
-end
-#
-image = image ./ trapz(rmesh, image)
-
-# Write spectral function
-open("image.data", "w") do fout
-    for i in eachindex(image)
-        @printf(fout, "%20.16f %20.16f\n", rmesh[i], image[i])
-    end
-end
-
-#
-# For Matsubara frequency data
-#
+ω = collect(LinRange(wmin, wmax, nmesh))
 
 # Matsubara frequency mesh
-iw = π / beta * (2.0 * collect(0:niw-1) .+ 0.0)
+iωₙ = π / beta * (2.0 * collect(0:niw-1) .+ 0.0)
 
 # Noise
 seed = rand(1:100000000)
 rng = MersenneTwister(seed)
 noise_ampl = 1.0e-4
-noise = randn(rng, F64, niw) * noise_ampl
-
-# Kernel function
-kernel = reshape(rmesh .^ 2.0, (1,nmesh)) ./
-         (reshape(iw .^ 2.0, (niw,1)) .+ reshape(rmesh .^ 2.0, (1,nmesh)))
-kernel[1,1] = 1.0
+noise_abs = randn(rng, F64, niw) * noise_ampl
+noise_phase = rand(rng, niw) * 2.0 * π
+noise = noise_abs .* exp.(noise_phase * im)
 
 # Build green's function
-KA = kernel .* reshape(image, (1,nmesh))
-chiw = zeros(F64, niw)
-for i in eachindex(chiw)
-    chiw[i] = trapz(rmesh, KA[i,:]) + noise[i]
+giw = zeros(C64, niw)
+giw2 = zeros(C64, niw)
+for i in eachindex(giw)
+    giw[i] = (
+        A₁ / ((iωₙ[i] * im) ^ 2.0 - ϵ₁ ^ 2.0) +
+        A₂ / ((iωₙ[i] * im) ^ 2.0 - ϵ₂ ^ 2.0)
+    )
+    giw2[i] = (
+        B₁ / (iωₙ[i] * im - ϵ₁) + (-B₁) / (iωₙ[i] * im + ϵ₁) +
+        B₂ / (iωₙ[i] * im - ϵ₂) + (-B₂) / (iωₙ[i] * im + ϵ₂)
+    )
 end
-norm = chiw[1]
-chiw = chiw / norm
+#
+gre = zeros(C64, nmesh)
+gre2 = zeros(C64, nmesh)
+for i in eachindex(gre)
+    gre[i] = (
+        A₁ / ((ω[i] + η * im) ^ 2.0 - ϵ₁ ^ 2.0) +
+        A₂ / ((ω[i] + η * im) ^ 2.0 - ϵ₂ ^ 2.0)
+    )
+    gre2[i] = (
+        B₁ / (ω[i] + η * im - ϵ₁) + (-B₁) / (ω[i] + η * im + ϵ₁) +
+        B₂ / (ω[i] + η * im - ϵ₂) + (-B₂) / (ω[i] + η * im + ϵ₂)
+    )
+end
 
 # Build error
-err = ones(F64, niw) * noise_ampl / norm
+err = ones(F64, niw) * noise_ampl
 
-# Write green's function
-open("chiw.data", "w") do fout
-    for i in eachindex(chiw)
-        z = chiw[i]
-        @printf(fout, "%20.16f %20.16f %20.16f\n", iw[i], z, err[i])
+# Write green's function (Matsubara frequency axis)
+open("giw.data", "w") do fout
+    for i in eachindex(giw)
+        z = giw[i]
+        @printf(fout, "%20.16f %20.16f %20.16f %20.16f\n", iωₙ[i], real(z), imag(z), err[i])
     end
 end
 
-#
-# For imaginary time data
-#
-
-# Imaginary time mesh
-tmesh = collect(LinRange(0, beta, ntau))
-
-# Noise
-seed = rand(1:100000000)
-rng = MersenneTwister(seed)
-noise_ampl = 1.0e-4
-noise = randn(rng, F64, ntau) * noise_ampl
-
-# Build green's function
-chit = zeros(F64, ntau)
-for i = 1:ntau
-    tw = exp.(-tmesh[i] * rmesh)
-    bw = exp.(-beta * rmesh)
-    btw = exp.(-(beta - tmesh[i]) * rmesh)
-    K = 0.5 * rmesh .* (tw .+ btw) ./ (1.0 .- bw)
-    K[1] = 1.0 / beta
-    global KA = K .* image
-    chit[i] = trapz(rmesh, KA) + noise[i]
+open("giw2.data", "w") do fout
+    for i in eachindex(giw)
+        z = giw2[i]
+        @printf(fout, "%20.16f %20.16f %20.16f %20.16f\n", iωₙ[i], real(z), imag(z), err[i])
+    end
 end
 
-# Build error
-err = ones(F64, ntau) * noise_ampl
+# Write green's function (real frequency axis)
+open("gre.data", "w") do fout
+    for i in eachindex(gre)
+        z = gre[i]
+        @printf(fout, "%20.16f %20.16f %20.16f\n", ω[i], real(z), imag(z))
+    end
+end
 
-# Write green's function
-open("chit.data", "w") do fout
-    for i in eachindex(chit)
-        @printf(fout, "%16.12f %16.12f %16.12f\n", tmesh[i], chit[i], err[i])
+open("gre2.data", "w") do fout
+    for i in eachindex(gre)
+        z = gre2[i]
+        @printf(fout, "%20.16f %20.16f %20.16f\n", ω[i], real(z), imag(z))
     end
 end
