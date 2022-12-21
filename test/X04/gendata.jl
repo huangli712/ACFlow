8bb2d9e9c51639de5b47efac3d80f6748f4d5024
Li Huang <lihuang.dmft@gmail.com>
2022-12-21 17:28:52 +0800

    add X04

--- test/X04/gendata.jl
+++ test/X04/gendata.jl
@@ -1,70 +1,40 @@
 #!/usr/bin/env julia
 
-push!(LOAD_PATH, ENV["ACFLOW_HOME"])
-
-using Random
+using DelimitedFiles
 using Printf
-using ACFlow
 
-# Setup parameters
-wmin = -5.0  # Left boundary
-wmax = +5.0  # Right boundary
-nmesh = 2001 # Number of real-frequency points
-niw  = 10    # Number of Matsubara frequencies
-beta = 10.0  # Inverse temperature
-ϵ₁   = 0.50  # Parameters for gaussian peaks
-ϵ₂   = -2.5
-A₁   = 1.00
-A₂   = 0.30
-Γ₁   = 0.20
-Γ₂   = 0.80
+# Number of grid points for input data
+# The first twenty points are enough
+ngrid = 20
 
-# Real frequency mesh
-rmesh = collect(LinRange(wmin, wmax, nmesh))
-
-# Spectral function
-image = similar(rmesh)
-#
-@. image  = A₁ * exp(-(rmesh - ϵ₁) ^ 2.0 / (2.0 * Γ₁ ^ 2.0))
-@. image += A₂ * exp(-(rmesh - ϵ₂) ^ 2.0 / (2.0 * Γ₂ ^ 2.0))
-#
-image = image ./ trapz(rmesh, image)
-
-# Matsubara frequency mesh
-iw = π / beta * (2.0 * collect(0:niw-1) .+ 1.0)
-
-# Noise
-seed = rand(1:100000000)
-rng = MersenneTwister(seed)
-noise_ampl = 1.0e-4
-noise_abs = randn(rng, F64, niw) * noise_ampl
-noise_phase = rand(rng, niw) * 2.0 * π
-noise = noise_abs .* exp.(noise_phase * im)
-
-# Kernel function
-kernel = 1.0 ./ (im * reshape(iw, (niw,1)) .- reshape(rmesh, (1,nmesh)))
-
-# Build green's function
-KA = kernel .* reshape(image, (1,nmesh))
-giw = zeros(C64, niw)
-for i in eachindex(giw)
-    giw[i] = trapz(rmesh, KA[i,:]) + noise[i]
-end
-
-# Build error
-err = ones(F64, niw) * noise_ampl
+# Read Matsubara green's function from solver.grn.dat
+dlm = readdlm("solver.grn.dat")
+grid = dlm[1:ngrid,2]
+giw  = dlm[1:ngrid,3] + im * dlm[1:ngrid,4]
+err  = dlm[1:ngrid,5] + im * dlm[1:ngrid,6]
 
 # Write green's function
 open("giw.data", "w") do fout
-    for i in eachindex(giw)
-        z = giw[i]
-        @printf(fout, "%20.16f %20.16f %20.16f %20.16f\n", iw[i], real(z), imag(z), err[i])
+    for i in eachindex(grid)
+        zg = giw[i]
+        ze = err[i]
+        @printf(fout, "%20.16f %20.16f %20.16f %20.16f %20.16f\n",
+            grid[i], real(zg), imag(zg), real(ze), imag(ze))
     end
 end
 
-# Write spectral function
-open("image.data", "w") do fout
-    for i in eachindex(image)
-        @printf(fout, "%20.16f %20.16f\n", rmesh[i], image[i])
+# Read Matsubara self-energy function from solver.sgm.dat
+dlm = readdlm("solver.sgm.dat")
+grid = dlm[1:ngrid,2]
+siw  = dlm[1:ngrid,3] + im * dlm[1:ngrid,4]
+err  = dlm[1:ngrid,5] + im * dlm[1:ngrid,6]
+
+# Write self-energy function
+open("siw.data", "w") do fout
+    for i in eachindex(grid)
+        zs = siw[i]
+        ze = err[i]
+        @printf(fout, "%20.16f %20.16f %20.16f %20.16f %20.16f\n",
+            grid[i], real(zs), imag(zs), real(ze), imag(ze))
     end
 end
---
+++ test/X04/sigma.jl
@@ -0,0 +1,68 @@
+#!/usr/bin/env julia
+
+push!(LOAD_PATH, ENV["ACFLOW_HOME"])
+
+using DelimitedFiles
+using Printf
+using ACFlow
+
+welcome()
+
+# Deal with self-energy function
+#
+# Read self-energy function
+dlm = readdlm("siw.data")
+#
+# Get grid
+grid = dlm[:,1]
+#
+# Get self-energy function
+Σinp = dlm[:,2] + im * dlm[:,3] # Value part
+Σerr = dlm[:,4] + im * dlm[:,5] # Error part
+#
+# Subtract hartree term
+Σ∞  = 1.0
+@. Σinp = Σinp - Σ∞
+
+# For MaxEnt solver
+
+# Setup parameters
+#
+# For [BASE] block
+# See types.jl/_PBASE for default setup
+B = Dict{String,Any}(
+    "solver" => "MaxEnt",  # Choose MaxEnt solver
+    "mtype"  => "gauss",   # Default model function
+    "mesh"   => "tangent", # Mesh for spectral function
+    "ngrid"  => 20,        # Number of grid points for input data
+    "nmesh"  => 801,       # Number of mesh points for output data
+    "wmax"   => 8.0,       # Right boundary of mesh
+    "wmin"   => -8.0,      # Left boundary of mesh
+    "beta"   => 10.0,      # Inverse temperature
+)
+#
+# For [MaxEnt] block
+# See types.jl/_PMaxEnt for default setup
+S = Dict{String,Any}(
+    "nalph"  => 15,        # Number of α parameters
+    "alpha"  => 1e12,      # Starting value of α parameter
+    "blur"   => -1.0,      # Enable preblur or not
+)
+#
+setup_param(B, S)
+
+# Call the solver
+mesh, Aout, Σout = solve(grid, Σinp, Σerr)
+
+# Calculate final self-energy function on real axis
+#
+# Construct final self-energy function
+@. Σout = Σout + Σ∞
+#
+# Write self-energy function to sigma.data
+open("sigma.data", "w") do fout
+    for i in eachindex(mesh)
+        z = Σout[i]
+        @printf(fout, "%20.16f %20.16f %20.16f\n", mesh[i], real(z), imag(z))
+    end
+end
