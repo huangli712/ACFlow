94fb589a6befc4d23a223fb97490fa3b2c5fdcf7
Li Huang <lihuang.dmft@gmail.com>
2022-12-24 10:37:35 +0800

    update test

--- test/X08/gendata.jl
+++ test/X08/gendata.jl
@@ -7,31 +7,19 @@ using Printf
 using ACFlow
 
 # Setup parameters
-wmin = -4.0 # Left boundary
-wmax = +4.0 # Right boundary
+wmin = -5.0  # Left boundary
+wmax = +5.0  # Right boundary
 nmesh = 2001 # Number of real-frequency points
 niw  = 10    # Number of Matsubara frequencies
-beta = 20.0  # Inverse temperature
-ϵ₀   = 36.0 / 10  # Parameters for δ-like peaks
-ϵ₁   = 12.0 / 10
-ϵ₂   = 2.00 / 10
-ϵ₃   = 0.00 / 10
-ϵ₄   =-2.00 / 10
-ϵ₅   =-12.0 / 10
-ϵ₆   =-24.0 / 10
-ϵ₇   =-36.0 / 10
-ϵ₈   =-8.00 / 10
-ϵ₉   = 10.0 / 10
-A₀   = 0.10
-A₁   = 0.10
-A₂   = 0.10
-A₃   = 0.10
-A₄   = 0.10
-A₅   = 0.10
-A₆   = 0.10
-A₇   = 0.10
-A₈   = 0.10
-A₉   = 0.10
+beta = 10.0  # Inverse temperature
+ϵ₁   = 4.00  # Parameters for δ-like peaks
+ϵ₂   = -4.0
+ϵ₃   = 1.00
+ϵ₄   = -1.0
+A₁   = 0.25
+A₂   = 0.25
+A₃   = 0.25
+A₄   = 0.25
 η    = 1e-2
 
 # Real frequency mesh
@@ -52,32 +40,20 @@ noise = noise_abs .* exp.(noise_phase * im)
 giw = zeros(C64, niw)
 for i in eachindex(giw)
     giw[i] = (
-        A₀ / (iωₙ[i] * im - ϵ₀) +
         A₁ / (iωₙ[i] * im - ϵ₁) +
         A₂ / (iωₙ[i] * im - ϵ₂) +
         A₃ / (iωₙ[i] * im - ϵ₃) +
-        A₄ / (iωₙ[i] * im - ϵ₄) +
-        A₅ / (iωₙ[i] * im - ϵ₅) +
-        A₆ / (iωₙ[i] * im - ϵ₆) +
-        A₇ / (iωₙ[i] * im - ϵ₇) +
-        A₈ / (iωₙ[i] * im - ϵ₈) +
-        A₉ / (iωₙ[i] * im - ϵ₉) + noise[i]
+        A₄ / (iωₙ[i] * im - ϵ₄) + noise[i]
     )
 end
 #
 gre = zeros(C64, nmesh)
 for i in eachindex(gre)
     gre[i] = (
-        A₀ / (ω[i] + η * im - ϵ₀) +
         A₁ / (ω[i] + η * im - ϵ₁) +
         A₂ / (ω[i] + η * im - ϵ₂) +
         A₃ / (ω[i] + η * im - ϵ₃) +
-        A₄ / (ω[i] + η * im - ϵ₄) +
-        A₅ / (ω[i] + η * im - ϵ₅) +
-        A₆ / (ω[i] + η * im - ϵ₆) +
-        A₇ / (ω[i] + η * im - ϵ₇) +
-        A₈ / (ω[i] + η * im - ϵ₈) +
-        A₉ / (ω[i] + η * im - ϵ₉)
+        A₄ / (ω[i] + η * im - ϵ₄)
     )
 end
 
