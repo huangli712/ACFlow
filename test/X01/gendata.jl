0a3d0402bab42bdd7fcf8cf800ff517c6d0c6f9f
Li Huang <lihuang.dmft@gmail.com>
2022-12-25 10:10:24 +0800

    fix gendata.jl

--- test/X01/gendata.jl
+++ test/X01/gendata.jl
@@ -12,9 +12,12 @@ wmax = +5.0  # Right boundary
 nmesh = 2001 # Number of real-frequency points
 niw  = 10    # Number of Matsubara frequencies
 beta = 10.0  # Inverse temperature
-ϵ₁   = 0.00  # Parameters for gaussian peaks
-A₁   = 1.00
-Γ₁   = 0.80
+ϵ₁   = 2.50  # Parameters for gaussian peaks
+ϵ₂   = -2.5
+A₁   = 0.50
+A₂   = 0.50
+Γ₁   = 0.50
+Γ₂   = 0.50
 
 # Real frequency mesh
 rmesh = collect(LinRange(wmin, wmax, nmesh))
@@ -22,7 +25,8 @@ rmesh = collect(LinRange(wmin, wmax, nmesh))
 # Spectral function
 image = similar(rmesh)
 #
-@. image = A₁ * exp(-(rmesh - ϵ₁) ^ 2.0 / (2.0 * Γ₁ ^ 2.0))
+@. image  = A₁ * exp(-(rmesh - ϵ₁) ^ 2.0 / (2.0 * Γ₁ ^ 2.0))
+@. image += A₂ * exp(-(rmesh - ϵ₂) ^ 2.0 / (2.0 * Γ₂ ^ 2.0))
 #
 image = image ./ trapz(rmesh, image)
 
