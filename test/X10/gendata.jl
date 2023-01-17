7d8899fdaab3ebb4d5bd874546538bb370a6c194
Li Huang <lihuang.dmft@gmail.com>
2023-01-17 17:34:01 +0800

    fix X10

--- test/X10/gendata.jl
+++ test/X10/gendata.jl
@@ -7,13 +7,17 @@ using Printf
 using ACFlow
 
 # Setup parameters
-wmin = +0.0  # Left boundary
-wmax = +8.0  # Right boundary
+wmin = -5.0  # Left boundary
+wmax = +5.0  # Right boundary
 nmesh = 2001 # Number of real-frequency points
-niw  = 20    # Number of Matsubara frequencies
-beta = 20.0  # Inverse temperature
-ϵ₁   = 2.00  # Parameters for gaussian peaks
-ϵ₂   = -2.0
+niw  = 10    # Number of Matsubara frequencies
+beta = 10.0  # Inverse temperature
+ϵ₁   = 2.50  # Parameters for gaussian peaks
+ϵ₂   = -2.5
+A₁   = 0.50
+A₂   = 0.50
+Γ₁   = 0.50
+Γ₂   = 0.50
 
 # Real frequency mesh
 rmesh = collect(LinRange(wmin, wmax, nmesh))
@@ -21,42 +25,40 @@ rmesh = collect(LinRange(wmin, wmax, nmesh))
 # Spectral function
 image = similar(rmesh)
 #
-@. image  = exp(-(rmesh - ϵ₁) ^ 2.0 / 2.0)
-@. image += exp(-(rmesh - ϵ₂) ^ 2.0 / 2.0)
+@. image  = A₁ * exp(-(rmesh - ϵ₁) ^ 2.0 / (2.0 * Γ₁ ^ 2.0))
+@. image += A₂ * exp(-(rmesh - ϵ₂) ^ 2.0 / (2.0 * Γ₂ ^ 2.0))
 #
 image = image ./ trapz(rmesh, image)
 
 # Matsubara frequency mesh
-iw = π / beta * (2.0 * collect(0:niw-1) .+ 0.0)
+iw = π / beta * (2.0 * collect(0:niw-1) .+ 1.0)
 
 # Noise
 seed = rand(1:100000000)
 rng = MersenneTwister(seed)
 noise_ampl = 1.0e-4
-noise = randn(rng, F64, niw) * noise_ampl
+noise_abs = randn(rng, F64, niw) * noise_ampl
+noise_phase = rand(rng, niw) * 2.0 * π
+noise = noise_abs .* exp.(noise_phase * im)
 
 # Kernel function
-kernel = reshape(rmesh .^ 2.0, (1,nmesh)) ./
-         (reshape(iw .^ 2.0, (niw,1)) .+ reshape(rmesh .^ 2.0, (1,nmesh)))
-kernel[1,1] = 1.0
+kernel = 1.0 ./ (im * reshape(iw, (niw,1)) .- reshape(rmesh, (1,nmesh)))
 
 # Build green's function
 KA = kernel .* reshape(image, (1,nmesh))
-chiw = zeros(F64, niw)
-for i in eachindex(chiw)
-    chiw[i] = trapz(rmesh, KA[i,:]) + noise[i]
+giw = zeros(C64, niw)
+for i in eachindex(giw)
+    giw[i] = trapz(rmesh, KA[i,:]) + noise[i]
 end
-norm = chiw[1]
-chiw = chiw / norm
 
 # Build error
-err = ones(F64, niw) * noise_ampl / norm
+err = ones(F64, niw) * noise_ampl
 
 # Write green's function
-open("chiw.data", "w") do fout
-    for i in eachindex(chiw)
-        z = chiw[i]
-        @printf(fout, "%20.16f %20.16f %20.16f\n", iw[i], z, err[i])
+open("giw.data", "w") do fout
+    for i in eachindex(giw)
+        z = giw[i]
+        @printf(fout, "%20.16f %20.16f %20.16f %20.16f\n", iw[i], real(z), imag(z), err[i])
     end
 end
 
