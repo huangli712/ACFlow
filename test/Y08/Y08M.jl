1c5f644e6ede6dc71038188634a0c102dc18383b
Li Huang <lihuang.dmft@gmail.com>
2023-01-26 23:35:46 +0800

    update Y08M.jl

--- test/Y08/Y08M.jl
+++ test/Y08/Y08M.jl
@@ -6,6 +6,22 @@ using ACFlow
 
 welcome()
 
+nkpt = 151
+niw = 10
+chiw = zeros(C64, nkpt, niw)
+grid = zeros(F64, niw)
+
+open("chiw.data", "r") do fin
+    for k = 1:nkpt
+        for i = 1:niw
+            arr = line_to_array(fin)
+            _re, _im = parse.(F64, arr[3:4])
+            grid[i] = parse(F64, arr[2])
+            chiw[k,i] = _re + _im * im
+        end
+    end
+end
+
 # For MaxEnt solver
 
 # Setup parameters
@@ -13,54 +29,33 @@ welcome()
 # For [BASE] block
 # See types.jl/_PBASE for default setup
 B = Dict{String,Any}(
-    "finput" => "siw.inp",
-    "mtype"  => "2lorentz",
-    "ngrid"  => 100,
-    "wmax"   => 20.0,
-    "wmin"   => -20.0,
-    "beta"   => 5.0,
-    "pmodel" => [2.0, -5.0, 5.0],
+    "finput" => "chiw.data",
+    "solver" => "MaxEnt",
+    "ktype"  => "bsymm",
+    "mtype"  => "flat",
+    "grid"   => "bfreq",
+    "mesh"   => "linear",
+    "ngrid"  => 10,
+    "nmesh"  => 501,
+    "wmax"   => 2.5,
+    "wmin"   => 0.0,
+    "beta"   => 50.0,
 )
 #
 # For [MaxEnt] block
 # See types.jl/_PMaxEnt for default setup
 S = Dict{String,Any}(
-    "nalph"  => 15,
+    "method" => "chi2kink",
+    "nalph"  => 14,
     "alpha"  => 1e12,
 )
 #
 setup_param(B, S)
 
 # Call the solver
-mesh, Aout, Gout = solve(read_data())
+mesh, Aout, Gout = solve(grid, chiw[1,:])
 
 # Backup calculated results
 cp("Aout.data", "Aout.mem.data", force = true)
 cp("Gout.data", "Gout.mem.data", force = true)
 cp("repr.data", "repr.mem.data", force = true)
-
-# For StochOM solver
-
-# Setup parameters
-#
-# For [BASE] block
-# See types.jl/_PBASE for default setup
-B = Dict{String,Any}(
-    "solver" => "StochOM"
-)
-#
-# For [StochOM] block
-# See types.jl/_PStochOM for default setup
-S = Dict{String,Any}(
-    "ntry"   => 100000
-)
-#
-setup_param(B, S, false)
-
-# Call the solver
-mesh, Aout, Gout = solve(read_data())
-
-# Backup calculated results
-cp("Aout.data", "Aout.som.data", force = true)
-cp("Gout.data", "Gout.som.data", force = true)
-cp("repr.data", "repr.som.data", force = true)
