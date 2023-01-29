5026cf5d8208b49a4457fcfba77182020111d214
Li Huang <lihuang.dmft@gmail.com>
2023-01-29 10:36:27 +0800

    add Y09

---
+++ test/Y09/gendata.jl
@@ -0,0 +1,110 @@
+#!/usr/bin/env julia
+
+push!(LOAD_PATH, ENV["ACFLOW_HOME"])
+
+using Random
+using Printf
+using ACFlow
+
+# Setup parameters
+wmin = -5.0  # Left boundary
+wmax = +5.0  # Right boundary
+nmesh = 2001 # Number of real-frequency points
+niw  = 10    # Number of Matsubara frequencies
+ntau = 1000  # Number of imaginary time points
+beta = 10.0  # Inverse temperature
+Δ    = 0.50  # 2Δ is the size of the gap
+W    = 6.00  # Bandwidth of the spectrum
+
+#
+# For true spectrum
+#
+
+# Real frequency mesh
+rmesh = collect(LinRange(wmin, wmax, nmesh))
+
+# Spectral function
+image = similar(rmesh)
+#
+for i in eachindex(rmesh)
+    image[i] = 0.0
+    if Δ < abs(rmesh[i]) < W/2
+        image[i] = abs(rmesh[i]) / sqrt(rmesh[i] ^ 2.0 - Δ ^ 2.0) / W
+    end
+end
+#
+image = image ./ trapz(rmesh, image)
+
+# Write spectral function
+open("image.data", "w") do fout
+    for i in eachindex(image)
+        @printf(fout, "%20.16f %20.16f\n", rmesh[i], image[i])
+    end
+end
+
+#
+# For Matsubara frequency data
+#
+
+# Matsubara frequency mesh
+iw = π / beta * (2.0 * collect(0:niw-1) .+ 1.0)
+
+# Noise
+seed = rand(1:100000000)
+rng = MersenneTwister(seed)
+noise_ampl = 1.0e-4
+noise_abs = randn(rng, F64, niw) * noise_ampl
+noise_phase = rand(rng, niw) * 2.0 * π
+noise = noise_abs .* exp.(noise_phase * im)
+
+# Kernel function
+kernel = 1.0 ./ (im * reshape(iw, (niw,1)) .- reshape(rmesh, (1,nmesh)))
+
+# Build green's function
+KA = kernel .* reshape(image, (1,nmesh))
+giw = zeros(C64, niw)
+for i in eachindex(giw)
+    giw[i] = trapz(rmesh, KA[i,:]) + noise[i]
+end
+
+# Build error
+err = ones(F64, niw) * noise_ampl
+
+# Write green's function
+open("giw.data", "w") do fout
+    for i in eachindex(giw)
+        z = giw[i]
+        @printf(fout, "%20.16f %20.16f %20.16f %20.16f\n", iw[i], real(z), imag(z), err[i])
+    end
+end
+
+#
+# For imaginary time data
+#
+
+# Imaginary time mesh
+tmesh = collect(LinRange(0, beta, ntau))
+
+# Noise
+seed = rand(1:100000000)
+rng = MersenneTwister(seed)
+noise_ampl = 1.0e-4
+noise = randn(rng, F64, ntau) * noise_ampl
+
+# Build green's function
+gtau = zeros(F64, ntau)
+for i = 1:ntau
+    tw = exp.(-tmesh[i] * rmesh)
+    bw = exp.(-beta * rmesh)
+    gtau[i] = trapz(rmesh, image .* tw ./ (1.0 .+ bw)) + noise[i]
+end
+
+# Build error
+err = ones(F64, ntau) * noise_ampl * 10.0
+
+# Write green's function
+open("gtau.data", "w") do fout
+    for i in eachindex(gtau)
+        @printf(fout, "%16.12f %16.12f %16.12f\n", tmesh[i], gtau[i], err[i])
+    end
+end
