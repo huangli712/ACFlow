70424903803662de72f8d7f3316bff324c89f3e4
Li Huang <lihuang.dmft@gmail.com>
2023-03-31 23:01:04 +0800

    update X01

--- test/devel/X01/test.py
+++ test/devel/X01/test.py
@@ -7,8 +7,8 @@ f = scipy.interpolate.interp1d(simage, mesh)
 
 smax = numpy.amax(simage)
 smin = numpy.amin(simage)
-y = numpy.linspace(smin, smax, 1000)
+y = numpy.linspace(smin, smax, 100000)
 x = f(y)
 
-for i in range(1000):
+for i in range(100000):
     print(i, x[i])
