3706b9d47f27289bc6f8956965f4e14eeb8a8b49
Li Huang <lihuang.dmft@gmail.com>
2023-04-01 18:03:26 +0800

    update gmesh.jl

--- util/gmesh.jl
+++ util/gmesh.jl
@@ -13,8 +13,43 @@ push!(LOAD_PATH, ENV["ACFLOW_HOME"])
 
 using ACFlow
 
+function read_spectrum(fn::String = "Aout.data")
+    nmesh = get_b("nmesh")
+
+    mesh = zeros(F64, nmesh)
+    image = zeros(F64, nmesh)
+
+    if isfile(fn)
+        open(fn, "r") do fin
+            for i = 1:nmesh
+                arr = line_to_array(fin)
+                mesh[i], image[i] = parse.(F64, arr)
+            end
+        end
+    else
+        error("Sorry, $fn does not exist!")
+    end
+
+    return mesh, image
+end
+
+function generate_mesh(mesh::Vector{F64}, image::Vector{F64})
+    nfine = 100000
+    Asum = cumsum(abs.(image))
+    f = LinearInterpolation(mesh, Asum)
+
+    vmax = maximum(Asum)
+    vmin = minimum(Asum)
+    Amesh = collect(LinRange(vmin, vmax, nfine))
+    fmesh = f.(Amesh)
+    @show fmesh
+    @show mesh
+end
+
 welcome()
 overview()
 read_param()
+
+generate_mesh(read_spectrum()...)
 
 goodbye()
