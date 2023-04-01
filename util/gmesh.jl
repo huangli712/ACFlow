1d2f593bf61004eade175277cf8eb0229144ffcc
Li Huang <lihuang.dmft@gmail.com>
2023-04-01 17:42:18 +0800

    fix gmesh.jl

--- util/gmesh.jl
+++ util/gmesh.jl
@@ -1,12 +1,12 @@
 #!/usr/bin/env julia
 
 #
-# This script is used to start analytical continuation simulations.
-# It will launch only 1 process.
+# This script is used to generate non-uniform mesh according to the
+# given spectral function. It will launch only 1 process.
 #
 # Usage:
 #
-#     $ acrun.jl ac.toml
+#     $ gmesh.jl ac.toml
 #
 
 push!(LOAD_PATH, ENV["ACFLOW_HOME"])
