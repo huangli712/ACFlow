b8a3c86e5f341bae3bdcc1d74af62ffee3bca658
Li Huang <lihuang.dmft@gmail.com>
2023-04-01 17:40:55 +0800

    add gmesh.jl

---
+++ util/gmesh.jl
@@ -0,0 +1,20 @@
+#!/usr/bin/env julia
+
+#
+# This script is used to start analytical continuation simulations.
+# It will launch only 1 process.
+#
+# Usage:
+#
+#     $ acrun.jl ac.toml
+#
+
+push!(LOAD_PATH, ENV["ACFLOW_HOME"])
+
+using ACFlow
+
+welcome()
+overview()
+read_param()
+
+goodbye()
