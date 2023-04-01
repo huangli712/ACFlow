bb9f7249c0fa8a9509a58eeecde80bed4503beb2
Li Huang <lihuang.dmft@gmail.com>
2023-04-01 11:05:02 +0800

    fix version number

--- src/global.jl
+++ src/global.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2023/03/27
+# Last modified: 2023/04/01
 #
 
 #=
@@ -121,7 +121,7 @@ Version of this julia toolkit.
 
 See also: [`__RELEASE__`](@ref).
 """
-const __VERSION__ = v"1.4.2-devel.230327"
+const __VERSION__ = v"1.4.3-devel.230401"
 
 """
     __RELEASE__
@@ -130,7 +130,7 @@ Release date of this julia toolkit.
 
 See also: [`__AUTHORS__`](@ref).
 """
-const __RELEASE__ = "2023/03"
+const __RELEASE__ = "2023/04"
 
 #=
 *Remarks* :
