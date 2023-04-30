2437f97efb0db76ae68a83c430135b9beb09ec83
Li Huang <lihuang.dmft@gmail.com>
2023-04-30 23:53:38 +0800

    add gendata.jl

--- test/future/X10/gendata.jl
+++ test/future/X10/gendata.jl
@@ -25,6 +25,14 @@ A₂   = 1.00
 # Matsubara frequency mesh
 iωₙ = π / beta * (2.0 * collect(0:niw-1) .+ 1.0)
 
+# Noise
+seed = rand(1:100000000)
+rng = MersenneTwister(seed)
+noise_ampl = 1.0e-4
+noise_abs = randn(rng, F64, niw) * noise_ampl
+noise_phase = rand(rng, niw) * 2.0 * π
+noise = noise_abs .* exp.(noise_phase * im)
+
 # Initial green's function (in Matsubara axis)
 giw1 = zeros(C64, niw)
 for i in eachindex(giw1)
