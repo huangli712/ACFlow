c4da04960839b0a69ca9a93e62be5057f373cdd3
Li Huang <lihuang.dmft@gmail.com>
2023-05-12 10:42:05 +0800

    add A11 and A12

---
+++ test/basic/A12/gendata.jl
@@ -0,0 +1,74 @@
+#!/usr/bin/env julia
+
+push!(LOAD_PATH, ENV["ACFLOW_HOME"])
+
+using Random
+using Printf
+using ACFlow
+
+# Setup parameters
+wmin = +0.0  # Left boundary
+wmax = +5.0  # Right boundary
+nmesh = 2001 # Number of real-frequency points
+niw  = 10    # Number of Matsubara frequencies
+beta = 10.0  # Inverse temperature
+ϵ₁   = 0.00  # Parameters for gaussian peaks
+ϵ₂   = 1.50
+ϵ₃   = -1.5
+A₁   = 1.00
+A₂   = 0.30
+A₃   = 0.30
+Γ₁   = 0.20
+Γ₂   = 0.80
+Γ₃   = 0.80
+
+# Real frequency mesh
+rmesh = collect(LinRange(wmin, wmax, nmesh))
+
+# Spectral function
+image = similar(rmesh)
+#
+@. image  = A₁ * exp(-(rmesh - ϵ₁) ^ 2.0 / (2.0 * Γ₁ ^ 2.0))
+@. image += A₂ * exp(-(rmesh - ϵ₂) ^ 2.0 / (2.0 * Γ₂ ^ 2.0))
+@. image += A₃ * exp(-(rmesh - ϵ₃) ^ 2.0 / (2.0 * Γ₃ ^ 2.0))
+#
+image = image ./ trapz(rmesh, image)
+
+# Matsubara frequency mesh
+iw = π / beta * (2.0 * collect(0:niw-1) .+ 0.0)
+
+# Noise
+seed = rand(1:100000000)
+rng = MersenneTwister(seed)
+noise_ampl = 1.0e-4
+noise = randn(rng, F64, niw) * noise_ampl
+
+# Kernel function
+kernel = -2.0 * reshape(rmesh .^ 2.0, (1,nmesh)) ./
+         (reshape(iw .^ 2.0, (niw,1)) .+ reshape(rmesh .^ 2.0, (1,nmesh)))
+kernel[1,1] = -2.0
+
+# Build green's function
+KA = kernel .* reshape(image, (1,nmesh))
+chiw = zeros(F64, niw)
+for i in eachindex(chiw)
+    chiw[i] = trapz(rmesh, KA[i,:]) + noise[i]
+end
+
+# Build error
+err = ones(F64, niw) * noise_ampl
+
+# Write green's function
+open("chiw.data", "w") do fout
+    for i in eachindex(chiw)
+        z = chiw[i]
+        @printf(fout, "%20.16f %20.16f %20.16f\n", iw[i], z, err[i])
+    end
+end
+
+# Write spectral function
+open("image.data", "w") do fout
+    for i in eachindex(image)
+        @printf(fout, "%20.16f %20.16f\n", rmesh[i], image[i])
+    end
+end
