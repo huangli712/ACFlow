ebf38563d69299c5b466690722b85f4dfeabb4c1
Li Huang <lihuang.dmft@gmail.com>
2023-09-27 21:20:46 +0800

    implement nac.jl

--- src/nac.jl
+++ src/nac.jl
@@ -423,6 +423,39 @@ function hardy_optim!(
     return causality, (Optim.converged(res))
 end
 
+"""
+Compute second derivative
+If the length of `x` and `y` is `N`, the length of the returned vector is `N-2`.
+"""
+function second_deriv(x::AbstractVector, y::AbstractVector)
+    if length(x) != length(y)
+        throw(ArgumentError("x and y must be the same length")) 
+    end
+
+    N = length(x)
+    dx_backward = view(x, 2:(N-1)) - view(x, 1:(N-2))
+    dx_forward = view(x, 3:N) - view(x, 2:(N-1))
+
+    y_forward = view(y, 3:N)
+    y_mid = view(y, 2:(N-1))
+    y_backward = view(y, 1:(N-2))
+
+    n = dx_backward .* y_forward + dx_forward .* y_backward - (dx_forward + dx_backward) .* y_mid
+    d = (dx_forward.^2) .* dx_backward + (dx_backward.^2) .* dx_forward
+    return 2 .* n ./ d
+end
+
+"""
+Integrate the squarre of the abs of the second derivative
+"""
+function integrate_squared_second_deriv(x::AbstractVector, y::AbstractVector)
+    N = length(x)
+    sd = second_deriv(x, y)
+
+    x_sd = view(x, 2:(N-1))
+    return integrate(x_sd, abs.(sd) .^ 2)
+end
+
 function integrate(x::AbstractVector, y::AbstractVector)
     N = length(x)
     dx = view(x, 2:N) .- view(x, 1:(N-1))
