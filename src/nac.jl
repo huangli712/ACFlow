98ce616aeac7b63e37879dae0edd5253d0298843
Li Huang <lihuang.dmft@gmail.com>
2023-11-02 11:34:28 +0800

    fix ACFlow.jl

--- src/nac.jl
+++ src/nac.jl
@@ -1531,11 +1531,6 @@ macro def(name, definition)
   end)
 end
 
-#@def add_linesearch_fields begin
-#    x_ls::Tx
-#    alpha::T
-#end
-
 @def initial_linesearch begin
     (similar(initial_x), # Buffer of x for line search in state.x_ls
     real(one(T)))             # Keep track of step size in state.alpha
