6e4c5c06343771c44e379c02658be9c44db3e5d6
Li Huang <lihuang.dmft@gmail.com>
2024-07-16 11:18:30 +0800

    fix chk_dict()

--- src/config.jl
+++ src/config.jl
@@ -4,7 +4,7 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2024/07/15
+# Last modified: 2024/07/16
 #
 
 """
@@ -469,8 +469,8 @@ function chk_dict()
         @case "StochOM"
             push!(PA, PStochOM)
             #
-            @assert get_s("ntry")  ≥ 40
-            @assert get_s("nstep") ≥ 100
+            @assert get_s("ntry")  ≥ 200
+            @assert get_s("nstep") ≥ 1000
             @assert get_s("nbox")  ≥ 100
             @assert get_s("sbox")  > 0.0
             @assert get_s("wbox")  > 0.0
