bbaca3798a3109999844bb980bbd6fb0971fc270
Li Huang <lihuang.dmft@gmail.com>
2024-07-27 00:42:51 +0800

    correct rfa.jl, add init()

--- src/rfa.jl
+++ src/rfa.jl
@@ -4,14 +4,28 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2024/07/26
+# Last modified: 2024/07/27
 #
 
 #=
 ### *Customized Structs* : *BarRat Solver*
 =#
 
+"""
+    BarRatContext
+
+Mutable struct. It is used within the BarRat solver only.
+
+### Members
+
+* Gᵥ   -> Input data for correlator.
+* grid -> Grid for input data.
+* mesh -> Mesh for output spectrum.
+"""
 mutable struct  BarRatContext
+    Gᵥ   :: Vector{C64}
+    grid :: AbstractGrid
+    mesh :: AbstractMesh
 end
 
 #=
@@ -34,7 +48,25 @@ function solve(S::BarRatSolver, rd::RawData)
     return brc.mesh.mesh, Aout, Gout
 end
 
+"""
+    init(S::BarRatSolver, rd::RawData)
+
+Initialize the BarRat solver and return a BarRatContext struct.
+"""
 function init(S::BarRatSolver, rd::RawData)
+    # Prepera input data
+    Gᵥ = rd.value
+    println("Postprocess input data: ", length(Gᵥ), " points")
+
+    # Prepare grid for input data
+    grid = make_grid(rd)
+    println("Build grid for input data: ", length(grid), " points")
+
+    # Prepare mesh for output spectrum
+    mesh = make_mesh()
+    println("Build mesh for spectrum: ", length(mesh), " points")
+
+    return BarRatContext(Gᵥ, grid, mesh)
 end
 
 function run(brc::BarRatContext)
