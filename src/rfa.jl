ae0ade1da5e23688e2bee2b00be1c520afaf8c68
Li Huang <lihuang.dmft@gmail.com>
2024-07-27 16:35:26 +0800

    fix rfa.jl

--- src/rfa.jl
+++ src/rfa.jl
@@ -7,6 +7,27 @@
 # Last modified: 2024/07/27
 #
 
+#
+# Note:
+#
+# The following codes for the Barycentric rational function approximation
+# are mostly adapted from
+#
+#     https://github.com/complexvariables/RationalFunctionApproximation.jl
+#
+# See
+#
+#     AAA Rational Approximation on a Continuum
+#     Tobin A. Driscoll, Yuji Nakatsukasa, Lloyd N. Trefethen
+#     SIAM Journal on Scientific Computing 46, A929 (2024) 
+#     
+# for more details.
+#
+
+#=
+### *Customized Structs* : *Barycentric*
+=#
+
 """
     Barycentric
 
@@ -87,20 +108,19 @@ function poles(r::Barycentric)
     m = length(w)
     B = diagm( [zero(T); ones(T, m)] )
     E = [zero(T) transpose(w); ones(T, m) diagm(z) ];
-    pol = []  # put it into scope
+    pol = [] # Put it into scope
     try
         pol = filter( isfinite, eigvals(E, B) )
     catch
-        # generalized eigen not available in extended precision, so:
+        # Generalized eigen not available in extended precision, so:
         λ = filter( z->abs(z)>1e-13, eigvals(E\B) )
         pol = 1 ./ λ
     end
     return pol
 end
 
-
 """
-    r(z)
+    (r::Barycentric)(z::Number)
 
 Evaluate the rational function at `z`.
 """
@@ -111,11 +131,140 @@ function (r::Barycentric)(z::Number)
     #
     k = findfirst(z .== r.nodes)
     #
-    if isnothing(k) # not at a node
+    if isnothing(k) # Not at a node
         C = @. 1 / (z - r.nodes)
         return sum(C .* r.w_times_f) / sum(C .* r.weights)
-    else            # interpolation at node
+    else            # Interpolation at node
         return r.values[k]
+    end
+end
+
+#=
+### *Adaptive Antoulas-Anderson Algorithm*
+=#
+
+"""
+    aaa(z, y)
+
+Adaptively compute a rational interpolant.
+
+### Arguments
+* `z::AbstractVector{<:Number}` -> Interpolation nodes.
+* `y::AbstractVector{<:Number}` -> Values at nodes.
+* `max_degree::Integer=150` -> Maximum numerator/denominator degree to use.
+* `float_type::Type=Float64` -> Floating point type to use for the computation.
+* `tol::Real=1000*eps(float_type)` -> Tolerance for stopping.
+* `lookahead::Integer=10` -> Number of iterations to determines stagnation.
+* `stats::Bool=false` -> Return convergence statistics.
+
+### Returns
+* `r::Barycentric` -> The rational interpolant.
+* `stats::NamedTuple` -> Convergence statistics, if keyword `stats=true`.
+
+### Examples
+```julia-repl
+julia> z = 1im * range(-10, 10, 500);
+
+julia> y = @. exp(z);
+
+julia> r = aaa(z, y);
+
+julia> degree(r)   # both numerator and denominator
+12
+
+julia> first(nodes(r), 4)
+4-element Vector{ComplexF64}:
+ 0.0 - 6.272545090180361im
+ 0.0 + 9.43887775551102im
+ 0.0 - 1.1022044088176353im
+ 0.0 + 4.909819639278557im
+
+julia> r(1im * π / 2)
+-2.637151617496356e-15 + 1.0000000000000002im
+```
+"""
+function aaa(
+    z::AbstractVector{<:Number},
+    y::AbstractVector{<:Number};
+    max_degree = 150,
+    float_type = Float64,
+    tol = 1000 * eps(float_type),
+    lookahead = 10,
+    stats = false
+    )
+
+    @assert float_type <: AbstractFloat
+    T = float_type
+    fmax = norm(y, Inf) # for scaling
+    m = length(z)
+    iteration = NamedTuple[]
+    err = T[]
+    besterr, bestidx, best = Inf, NaN, nothing
+
+    # Allocate space for Cauchy matrix, Loewner matrix, and residual
+    C = similar(z, (m, m))
+    L = similar(z, (m, m))
+    R = complex(zeros(size(z)))
+
+    ȳ = sum(y) / m
+    s, idx = findmax(abs(y - ȳ) for y in y)
+    push!(err, s)
+
+    # The ordering of nodes matters,
+    # while the order of test points does not.
+    node_index = Int[]
+    push!(node_index, idx)
+    test_index = Set(1:m)
+    delete!(test_index, idx)
+
+    n = 0 # Number of poles
+    while true
+        n += 1
+        σ = view(z, node_index)
+        fσ = view(y, node_index)
+        # Fill in matrices for the latest node
+        @inbounds @fastmath for i in test_index
+            δ = z[i] - σ[n]
+            # δ can be zero if there are repeats in z
+            C[i, n] = iszero(δ) ? 1 / eps() : 1 / δ
+            L[i, n] = (y[i] - fσ[n]) * C[i, n]
+        end
+        istest = collect(test_index)
+        _, _, V = svd( view(L, istest, 1:n) )
+        w = V[:, end] # Barycentric weights
+
+        CC = view(C, istest, 1:n)
+        num = CC * (w.*fσ)
+        den = CC * w
+        @. R[istest] = y[istest] - num / den
+        push!(err, norm(R, Inf))
+        push!(iteration, (; weights=w, active=copy(node_index)))
+
+        if (Base.last(err) < besterr)
+            besterr = Base.last(err)
+            bestidx = length(iteration)
+            best = Base.last(iteration)
+        end
+
+        # Are we done?
+        if (besterr <= tol*fmax) ||
+            (n == max_degree + 1) ||
+            ((length(iteration) - bestidx >= lookahead) && (besterr < 1e-2*fmax))
+            break
+        end
+
+        _, j = findmax(abs, R)
+        push!(node_index, j)
+        delete!(test_index, j)
+        R[j] = 0
+    end
+
+    idx, w = best.active, best.weights
+    r = Barycentric(z[idx], y[idx], w)
+    if stats
+        return r, (;err, iteration)
+    else
+        return r
     end
 end
 
@@ -212,128 +361,3 @@ function last(brc::BarRatContext)
     return Aout, _G
 end
 
-"""
-    aaa(z, y)
-
-Adaptively compute a rational interpolant.
-
-# Arguments
-
-## discrete mode
-- `z::AbstractVector{<:Number}`: interpolation nodes
-- `y::AbstractVector{<:Number}`: values at nodes
-
-## continuous mode
-- `f::Function`: function to approximate on the interval [-1,1]
-
-# Keyword arguments
-- `max_degree::Integer=150`: maximum numerator/denominator degree to use
-- `float_type::Type=Float64`: floating point type to use for the computation
-- `tol::Real=1000*eps(float_type)`: tolerance for stopping
-- `lookahead::Integer=10`: number of iterations to determines stagnation
-- `stats::Bool=false`: return convergence statistics
-
-# Returns
-- `r::Barycentric`: the rational interpolant
-- `stats::NamedTuple`: convergence statistics, if keyword `stats=true`
-
-# Examples
-```julia-repl
-julia> z = 1im * range(-10, 10, 500);
-
-julia> y = @. exp(z);
-
-julia> r = aaa(z, y);
-
-julia> degree(r)   # both numerator and denominator
-12
-
-julia> first(nodes(r), 4)
-4-element Vector{ComplexF64}:
- 0.0 - 6.272545090180361im
- 0.0 + 9.43887775551102im
- 0.0 - 1.1022044088176353im
- 0.0 + 4.909819639278557im
-
-julia> r(1im * π / 2)
--2.637151617496356e-15 + 1.0000000000000002im
-```
-
-See also [`approximate`](@ref) for approximating a function on a curve or region.
-"""
-function aaa(z::AbstractVector{<:Number}, y::AbstractVector{<:Number};
-    max_degree = 150, float_type = Float64, tol = 1000*eps(float_type),
-    lookahead = 10, stats = false
-    )
-
-    @assert float_type <: AbstractFloat
-    T = float_type
-    fmax = norm(y, Inf)    # for scaling
-    m = length(z)
-    iteration = NamedTuple[]
-    err = T[]
-    besterr, bestidx, best = Inf, NaN, nothing
-
-    # Allocate space for Cauchy matrix, Loewner matrix, and residual
-    C = similar(z, (m, m))
-    L = similar(z, (m, m))
-    R = complex(zeros(size(z)))
-
-    ȳ = sum(y) / m
-    s, idx = findmax(abs(y - ȳ) for y in y)
-    push!(err, s)
-
-    # The ordering of nodes matters, while the order of test points does not.
-    node_index = Int[]
-    push!(node_index, idx)
-    test_index = Set(1:m)
-    delete!(test_index, idx)
-
-    n = 0 # number of poles
-    while true
-        n += 1
-        σ = view(z, node_index)
-        fσ = view(y, node_index)
-        # Fill in matrices for the latest node
-        @inbounds @fastmath for i in test_index
-            δ = z[i] - σ[n]
-            # δ can be zero if there are repeats in z
-            C[i, n] = iszero(δ) ? 1 / eps() : 1 / δ
-            L[i, n] = (y[i] - fσ[n]) * C[i, n]
-        end
-        istest = collect(test_index)
-        _, _, V = svd( view(L, istest, 1:n) )
-        w = V[:, end]    # barycentric weights
-
-        CC = view(C, istest, 1:n)
-        num = CC * (w.*fσ)
-        den = CC * w
-        @. R[istest] = y[istest] - num / den
-        push!(err, norm(R, Inf))
-        push!(iteration, (; weights=w, active=copy(node_index)))
-
-        if (Base.last(err) < besterr)
-            besterr, bestidx, best = Base.last(err), length(iteration), Base.last(iteration)
-        end
-
-        # Are we done?
-        if (besterr <= tol*fmax) ||
-            (n == max_degree + 1) ||
-            ((length(iteration) - bestidx >= lookahead) && (besterr < 1e-2*fmax))
-            break
-        end
-
-        _, j = findmax(abs, R)
-        push!(node_index, j)
-        delete!(test_index, j)
-        R[j] = 0
-    end
-
-    idx, w = best.active, best.weights
-    r = Barycentric(z[idx], y[idx], w)
-    if stats
-        return r, (;err, iteration)
-    else
-        return r
-    end
-end
