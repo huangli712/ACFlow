42ca970c41922128f12c017318d38aaa966f0be6
Li Huang <lihuang.dmft@gmail.com>
2024-08-03 22:33:08 +0800

    add R07

--- test/rfa/R07/gendata.jl
+++ test/rfa/R07/gendata.jl
@@ -12,7 +12,7 @@ niw  = 100   # Number of Matsubara frequencies
 beta = 50.0  # Inverse temperature
 η    = 1e-2  # Parameters for δ-like peaks
 ϵ    = [-0.1, -0.3, 0.2, 0.4, -0.4, -0.2, 0.3, 0.1]
-A    = [-3.0, -2.0, -1.0, -0.02, 0.02, 1.0, 2.0, 3.0]
+A    = [-3.0, -2.0, -1.0, -0.2, 0.2, 1.0, 2.0, 3.0]
 
 # Real frequency mesh
 ω = collect(LinRange(wmin, wmax, nmesh))
