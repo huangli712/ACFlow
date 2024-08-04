9e3c72c3801fa9f498b12d0945de27b670e51098
Li Huang <lihuang.dmft@gmail.com>
2024-08-04 23:15:30 +0800

    fix test

--- test/rfa/R08/gendata.jl
+++ test/rfa/R08/gendata.jl
@@ -9,7 +9,6 @@ wmin = +0.0  # Left boundary
 wmax = +8.0  # Right boundary
 nmesh = 2001 # Number of real-frequency points
 niw  = 100   # Number of Matsubara frequencies
-ntau = 501   # Number of imaginary time points
 beta = 50.0  # Inverse temperature
 W₁   = 0.30  # Parameters for gaussian peaks
 W₂   = 0.20
@@ -77,40 +76,5 @@ open("chiw.data", "w") do fout
     for i in eachindex(chiw)
         z = chiw[i]
         @printf(fout, "%20.16f %20.16f %20.16f\n", iw[i], z, err[i])
-    end
-end
-
-#
-# For imaginary time data
-#
-
-# Imaginary time mesh
-tmesh = collect(LinRange(0, beta, ntau))
-
-# Noise
-seed = rand(1:100000000)
-rng = MersenneTwister(seed)
-noise_ampl = 1.0e-4
-noise = randn(rng, F64, ntau) * noise_ampl
-
-# Build green's function
-chit = zeros(F64, ntau)
-for i = 1:ntau
-    tw = exp.(-tmesh[i] * rmesh)
-    bw = exp.(-beta * rmesh)
-    btw = exp.(-(beta - tmesh[i]) * rmesh)
-    K = rmesh .* (tw .+ btw) ./ (1.0 .- bw)
-    K[1] = 2.0 / beta
-    global KA = K .* image
-    chit[i] = trapz(rmesh, KA) + noise[i]
-end
-
-# Build error
-err = ones(F64, ntau) * noise_ampl
-
-# Write green's function
-open("chit.data", "w") do fout
-    for i in eachindex(chit)
-        @printf(fout, "%16.12f %16.12f %16.12f\n", tmesh[i], chit[i], err[i])
     end
 end
