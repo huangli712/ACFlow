62fe5bbcc1f59869602820b8cc47a8139e63bf01
Li Huang <lihuang.dmft@gmail.com>
2024-09-23 16:25:07 +0800

    fix ppole.jl

--- util/ppole.jl
+++ util/ppole.jl
@@ -26,15 +26,13 @@ function parse_pole_data()
     P = zeros(I64, npole)
     A = zeros(F64, npole)
     𝕊 = zeros(F64, npole)
-    χ² = zeros(F64, ntry)
 
     fn = "pole.data"
     @assert isfile(fn)
 
     open(fn, "r") do fin
-        for i = 1:ntry
-            ldata = line_to_array(fin)
-            χ²[i] = parse(F64, ldata[5])
+        for _ = 1:ntry
+            readline(fin)
             for j = 1:npole
                 ldata = line_to_array(fin)
                 ind = parse(I64, ldata[1])
@@ -49,7 +47,7 @@ function parse_pole_data()
         end
     end
 
-    return χ², SPE
+    return SPE
 end
 
 function filter_pole_data()
