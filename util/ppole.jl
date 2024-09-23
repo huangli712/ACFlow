2e822016bdebe3180cf8303483a6877119a2b0da
Li Huang <lihuang.dmft@gmail.com>
2024-09-23 15:53:29 +0800

    fix ppole.jl

--- util/ppole.jl
+++ util/ppole.jl
@@ -11,7 +11,20 @@ push!(LOAD_PATH, "/Users/lihuang/Working/devel/ACFlow/src/")
 using Printf
 using ACFlow
 
+function calc_green_function()
+end
+
+function parse_pole_data()
+end
+
+function filter_pole_data()
+end
+
+function pole_to_green()
+end
+
 welcome()
 overview()
 read_param()
+pole_to_green()
 goodbye()
